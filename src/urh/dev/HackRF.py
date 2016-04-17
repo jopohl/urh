046698aaddbf0441ae7da6a2e2028537a0eac153
jopohl <Johannes.Pohl90@gmail.com>
2016-04-17 12:31:45 +0200

    Device Class

---
+++ src/urh/dev/HackRF.py
@@ -0,0 +1,44 @@
+from urh.dev.Device import Device
+from urh.cythonext import hackrf
+
+class HackRF(Device):
+    def __init__(self, bw, freq, gain, srate, initial_bufsize=8e9, is_ringbuffer=False):
+        super().__init__(bw, freq, gain, srate, initial_bufsize, is_ringbuffer)
+        self.is_open = False
+        self.success = 0
+
+    def open(self):
+        if not self.is_open:
+            if hackrf.setup() == self.success:
+                self.is_open = True
+                self.set_device_parameters()
+
+    def close(self):
+        if self.is_open:
+            if hackrf.exit() == self.success:
+               self.is_open = False
+
+    def start_rx_mode(self):
+        hackrf.start_rx_mode(self.callback_recv)
+
+    def stop_rx_mode(self, msg):
+        print("Stopping Rx-Mode: ", msg)
+        hackrf.stop_rx_mode()
+
+    def set_device_bandwidth(self, bw):
+        if self.is_open:
+            hackrf.set_baseband_filter_bandwidth(bw)
+
+    def set_device_frequency(self, value):
+        if self.is_open:
+            hackrf.set_freq(value)
+
+    def set_device_gain(self, gain):
+        if self.is_open:
+            hackrf.set_lna_gain(gain)
+            hackrf.set_vga_gain(gain)
+            hackrf.set_txvga_gain(gain)
+
+    def set_device_sample_rate(self, sample_rate):
+        if self.is_open:
+            hackrf.set_sample_rate(sample_rate)
--- tests/HackRF.py
+++ tests/HackRF.py
@@ -6,10 +6,13 @@ import time
 import numpy as np
 
 from urh.cythonext import hackrf
+from urh.dev.HackRF import HackRF
+
 
 class TestHackRF(unittest.TestCase):
     def callback_fun(self, buffer):
         out = []
+        print(buffer)
         for i in range(0,len(buffer), 4):
             try:
                 r = np.fromstring(buffer[i:i+2], dtype=np.float16)/32767.5
@@ -28,23 +31,31 @@ class TestHackRF(unittest.TestCase):
         hackrf.set_freq(433.92e6)
         print(hackrf.is_streaming())
         hackrf.start_rx_mode(self.callback_fun)
-        #time.sleep(1)
-        while hackrf.is_streaming():
-            pass
-         #   #time.sleep(0.1)
+        time.sleep(1)
         hackrf.stop_rx_mode()
 
     def test_fromstring(self):
-        buffer = b'\xfa\xfa\xff\xfa\xff\xf0\xff\xfa'
+        buffer = b'\xfe\xfe\xfe\xfe\xfe\xfe\xfe\xfd\xff\xfd\xfe\xfe\xfe\xfe\xfe\xfe\xfe\xfe\xfd\xfe\xfd\xfe\xff\xfe\xfe\xfe\xfe\xfe\xfe\xfe\xfd\xfe'
         r = np.empty(len(buffer)//4, dtype=np.float16)
         i = np.empty(len(buffer)//4, dtype=np.float16)
         c = np.empty(len(buffer)//4, dtype=np.complex64)
 
-        for j in range(0, len(buffer), 4):
-            r[j//4] = np.frombuffer(buffer[j:j + 2], dtype=np.float16) / 32767.5
-            i[j//4] = np.frombuffer(buffer[j + 2:j + 4], dtype=np.float16) / 32767.5
+        for j in range(0, len(buffer)-3, 4):
+            print(np.frombuffer(buffer[j:j + 2], dtype=np.float16))
+            r[j//4] = np.frombuffer(buffer[j:j + 2], dtype=np.uint8) / 32767.5
+            i[j//4] = np.frombuffer(buffer[j + 2:j + 4], dtype=np.uint8) / 32767.5
         #r2 = np.fromstring(buffer[], dtype=np.float16) / 32767.5
         c.real = r
         c.imag = i
         print(c)
         #x,y = np.frombuffer(buffer, dtype=[('x', np.float16), ('y', np.float16)])
+
+
+    def test_hackrf_class(self):
+        hfc = HackRF(1e6, 433e6, 20, 1e6)
+        hfc.open()
+        hfc.start_rx_mode()
+        time.sleep(10)
+        print(hfc.data[:hfc.current_index])
+        hfc.stop_rx_mode("Finished test")
+        #hfc.close()

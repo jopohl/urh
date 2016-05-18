6fc2f08250f6fb2ba19ec6c8b43f3ef76bfee652
jopohl <Johannes.Pohl90@gmail.com>
2016-05-18 17:51:14 +0200

    Participant enhancements

--- src/urh/signalprocessing/Participant.py
+++ src/urh/signalprocessing/Participant.py
@@ -2,15 +2,53 @@ import xml.etree.ElementTree as ET
 import string
 import random
 
-class Participant(object):
+from PyQt5.QtCore import QObject, pyqtSignal
+
+
+class Participant(QObject):
+    name_changed = pyqtSignal(str)
+    shortname_changed = pyqtSignal(str)
+    address_hex_changed = pyqtSignal(str)
+
     def __init__(self, name: str, shortname: str = None, address_hex: str = None, id: str = None):
-        self.name = name if name else "unknown"
-        self.shortname = shortname if shortname else name[0].upper() if len(name) > 0 else "X"
-        self.address_hex = address_hex if address_hex else ""
+        super().__init__()
+        self.__name = name if name else "unknown"
+        self.__shortname = shortname if shortname else name[0].upper() if len(name) > 0 else "X"
+        self.__address_hex = address_hex if address_hex else ""
         if id is None:
             self.__id = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(50))
         else:
             self.__id = id
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if value != self.__name:
+            self.__name = value
+            self.name_changed.emit(self.__name)
+
+    @property
+    def shortname(self) -> str:
+        return self.__shortname
+
+    @shortname.setter
+    def shortname(self, value: str):
+        if value != self.__shortname:
+            self.__shortname = value
+            self.shortname_changed.emit(self.__shortname)
+
+    @property
+    def address_hex(self) -> str:
+        return self.__address_hex
+
+    @address_hex.setter
+    def address_hex(self, value: str):
+        if self.__address_hex != value:
+            self.__address_hex = value
+            self.address_hex_changed.emit(self.__address_hex)
 
     def __repr__(self):
         return "Participant: {0} ({1}) addr: {2}".format(self.name, self.shortname, self.address_hex)
@@ -29,9 +67,7 @@ class Participant(object):
 
     @staticmethod
     def from_xml(tag: ET.Element):
-        result = Participant("empty", id = tag.attrib["id"])
-        result.name = tag.get("name", "Empty")
-        result.shortname = tag.get("shortname", "X")
-        result.address_hex = tag.get("address_hex", "")
-
-        return result
+        name = tag.get("name", "Empty")
+        shortname = tag.get("shortname", "X")
+        address_hex = tag.get("address_hex", "")
+        return Participant(name, shortname=shortname, address_hex=address_hex, id = tag.attrib["id"])

90ed53b3759b06c4f21ff3f45c64001af2493312
jopohl <Johannes.Pohl90@gmail.com>
2016-05-22 13:20:37 +0200

    fix #31

--- src/urh/util/Logger.py
+++ src/urh/util/Logger.py
@@ -12,7 +12,6 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
-
 logging.basicConfig(level=logging.WARNING, format='[%(levelname)s] %(message)s')
 logging.addLevelName(logging.WARNING, "{0}{1}{2}".format(color.YELLOW+color.BOLD, logging.getLevelName(logging.WARNING), color.END))
 logging.addLevelName(logging.ERROR, "{0}{1}{2}".format(color.RED+color.BOLD, logging.getLevelName(logging.ERROR), color.END))

2ff0c98bdc73f0ab13f100bb6577cb7be9359840
jopohl <Johannes.Pohl90@gmail.com>
2016-07-07 14:07:04 +0200

    use sys.executable for python3 interpreter

--- src/urh/cythonext/build.py
+++ src/urh/cythonext/build.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import sys
 import tempfile
 from subprocess import call
@@ -7,21 +6,8 @@ from subprocess import call
 sys.dont_write_bytecode = True
 build_dir = os.path.join(tempfile.gettempdir(), "build")
 
-def get_python3_interpreter():
-    paths = os.get_exec_path()
-
-    for p in paths:
-        for prog in ["python3", "python.exe"]:
-            attempt = os.path.join(p, prog)
-            if os.path.isfile(attempt):
-                return attempt
-
-    return None
-
 def main():
-    python3 = get_python3_interpreter()
-
-    call([python3, "setup.py", "build_ext",
+    call([sys.executable, "setup.py", "build_ext",
           "--build-lib",
           os.path.join(build_dir, "result"),
           "--build-temp", build_dir])

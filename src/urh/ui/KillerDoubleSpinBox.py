8f17c9fe3ea0edc910c86c6f102d47d9fe48d4f5
jopohl <Johannes.Pohl90@gmail.com>
2016-09-22 16:54:04 +0200

    fix #101

--- src/urh/ui/KillerDoubleSpinBox.py
+++ src/urh/ui/KillerDoubleSpinBox.py
@@ -10,45 +10,13 @@ class KillerDoubleSpinBox(QDoubleSpinBox):
         super().__init__(parent)
 
         self.lineEdit().setValidator(None)
-        self.lineEdit().textChanged.connect(self.on_text_changed)
-        self.editingFinished.connect(self.on_text_edited)
         self.editingFinished.connect(self.adjust_step)
-        self.valueChanged.connect(self.setUnit)
         self.auto_suffix = True
 
-    def on_text_changed(self):
-        text = self.lineEdit().text().upper()
-        if text.endswith("G") or text.endswith("M") or text.endswith("K"):
-            if self.suffix() != text[-1]:
-                if self.auto_suffix:
-                    self.setSuffix(text[-1])
-        else:
-            if self.suffix() != "":
-                if self.auto_suffix:
-                    self.setSuffix("")
-
-    def on_text_edited(self):
-        self.lineEdit().setText(self.lineEdit().text().upper())
 
     def setValue(self, val: float):
         super().setValue(val)
         self.adjust_step()
-
-
-    def setUnit(self):
-        value = abs(self.value())
-        if 10 ** 9 <= value <= 10 ** 11:
-            if self.suffix() != "G" and self.auto_suffix:
-                self.setSuffix("G")
-        elif 10 ** 6 <= value < 10 ** 9:
-            if self.suffix() != "M" and self.auto_suffix:
-                self.setSuffix("M")
-        elif 10 ** 3 <= value < 10 ** 6:
-            if self.suffix() != "K" and self.auto_suffix:
-                self.setSuffix("K")
-        else:
-            if self.suffix() != "" and self.auto_suffix:
-                self.setSuffix("")
 
     def adjust_step(self):
         value = abs(self.value())
@@ -63,11 +31,11 @@ class KillerDoubleSpinBox(QDoubleSpinBox):
 
     def textFromValue(self, value: float):
         if abs(value) >= 10 ** 9:
-            return super().textFromValue(value / 10 ** 9)
+            return super().textFromValue(value / 10 ** 9) + "G"
         elif abs(value) >= 10 ** 6:
-            return super().textFromValue(value / 10 ** 6)
+            return super().textFromValue(value / 10 ** 6) + "M"
         elif abs(value) >= 10 ** 3:
-            return super().textFromValue(value / 10 ** 3)
+            return super().textFromValue(value / 10 ** 3) + "K"
         else:
             return super().textFromValue(value)
 
@@ -82,6 +50,6 @@ class KillerDoubleSpinBox(QDoubleSpinBox):
             return super().valueFromText(text)
 
     def validate(self, inpt: str, pos: int):
-        rx = QRegExp("^(-?[0-9]+)[.]?[0-9]*[kKmMgG"+str(self.suffix())+"]?$")
+        rx = QRegExp("^(-?[0-9]+)[.]?[0-9]*[kKmMgG]?$")
         result = QValidator.Acceptable if rx.exactMatch(inpt.replace(",", ".")) else QValidator.Invalid
         return result, inpt, pos

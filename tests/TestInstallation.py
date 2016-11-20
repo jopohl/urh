23e8c0579ba00f07ef93bf9e5d3b1b8854b11a70
jopohl <Johannes.Pohl90@gmail.com>
2016-11-20 19:42:52 +0100

    remove rebuilds

--- tests/TestInstallation.py
+++ tests/TestInstallation.py
@@ -9,16 +9,16 @@ class TestInstallation(unittest.TestCase):
         self.assertTrue(docker_util.run_image("archlinux"))
 
     def test_debian8(self):
-        self.assertTrue(docker_util.run_image("debian8", True))
+        self.assertTrue(docker_util.run_image("debian8"))
 
     def test_ubuntu1404(self):
-        self.assertTrue(docker_util.run_image("ubuntu1404", True))
+        self.assertTrue(docker_util.run_image("ubuntu1404"))
 
     def test_ubuntu1604(self):
         self.assertTrue(docker_util.run_image("ubuntu1604"))
 
     def test_kali(self):
-        self.assertTrue(docker_util.run_image("kali", True))
+        self.assertTrue(docker_util.run_image("kali"))
 
     def tearDown(self):
         docker_util.remove_containers()

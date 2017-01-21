872bd9bf5dd12a741a116fa352351934a940b5c8
jopohl <Johannes.Pohl90@gmail.com>
2017-01-21 18:38:49 +0100

    add send and receive methods + tooltips

--- src/urh/plugins/NetworkSDRInterface/NetworkSDRInterfacePlugin.py
+++ src/urh/plugins/NetworkSDRInterface/NetworkSDRInterfacePlugin.py
@@ -1,3 +1,6 @@
+import socketserver
+import threading
+
 from PyQt5.QtCore import QRegExp
 from PyQt5.QtGui import QRegExpValidator
 import socket
@@ -6,6 +9,12 @@ from urh.plugins.Plugin import SDRPlugin
 
 
 class NetworkSDRInterfacePlugin(SDRPlugin):
+    class MyTCPHandler(socketserver.BaseRequestHandler):
+        def handle(self):
+            self.data = self.request.recv(1024)
+            print("{} wrote:".format(self.client_address[0]))
+            print(self.data)
+
     def __init__(self):
         super().__init__(name="NetworkSDRInterface")
         self.client_ip = self.qsettings.value("client_ip", defaultValue="127.0.0.1", type=str)
@@ -13,7 +22,6 @@ class NetworkSDRInterfacePlugin(SDRPlugin):
 
         self.client_port = self.qsettings.value("client_port", defaultValue=1338, type=int)
         self.server_port = self.qsettings.value("server_port", defaultValue=1337, type=int)
-
 
     def create_connects(self):
         ip_regex = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
@@ -29,6 +37,33 @@ class NetworkSDRInterfacePlugin(SDRPlugin):
         self.settings_frame.lineEditServerIP.editingFinished.connect(self.on_linedit_server_ip_editing_finished)
         self.settings_frame.spinBoxClientPort.editingFinished.connect(self.on_spinbox_client_port_editing_finished)
         self.settings_frame.spinBoxServerPort.editingFinished.connect(self.on_spinbox_server_port_editing_finished)
+
+    def start_tcp_server_for_receiving(self):
+        self.server = socketserver.TCPServer((self.server_ip, self.server_port), self.MyTCPHandler,
+                                             bind_and_activate=False)
+
+        self.server.allow_reuse_address = True  # allow reusing addresses if the server is stopped and started again
+        self.server.server_bind()      # only necessary, because we disabled bind_and_activate above
+        self.server.server_activate()  # only necessary, because we disabled bind_and_activate above
+
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread.daemon = True
+        self.server_thread.start()
+
+    def stop_tcp_server(self):
+        if hasattr(self, "server"):
+            self.server.shutdown()
+        if hasattr(self, "server_thread"):
+            self.server_thread.join()
+            print("thread down")
+
+
+    def send_data(self, data:bytearray):
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Connect to server and send data
+            sock.connect((self.client_ip, self.client_port))
+            sock.sendall(data)
 
     @staticmethod
     def bytearray_to_bit_str(arr: bytearray) -> str:

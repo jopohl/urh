333f86a9f5168d5b8becc1adb77b80d8be3cc21f
andynoack <andreas.noack@fh-stralsund.de>
2017-01-26 10:37:18 +0100

    improve network SDR plugin GUI

--- src/urh/plugins/NetworkSDRInterface/NetworkSDRInterfacePlugin.py
+++ src/urh/plugins/NetworkSDRInterface/NetworkSDRInterfacePlugin.py
@@ -25,7 +25,8 @@ class NetworkSDRInterfacePlugin(SDRPlugin):
     def __init__(self):
         super().__init__(name="NetworkSDRInterface")
         self.client_ip = self.qsettings.value("client_ip", defaultValue="127.0.0.1", type=str)
-        self.server_ip = self.qsettings.value("server_ip", defaultValue="127.0.0.1", type=str)
+        #self.server_ip = self.qsettings.value("server_ip", defaultValue="", type=str)
+        self.server_ip = ""
 
         self.client_port = self.qsettings.value("client_port", defaultValue=1338, type=int)
         self.server_port = self.qsettings.value("server_port", defaultValue=1337, type=int)
@@ -39,11 +40,12 @@ class NetworkSDRInterfacePlugin(SDRPlugin):
 
     def create_connects(self):
         ip_regex = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+
         self.settings_frame.lineEditClientIP.setValidator(QRegExpValidator(QRegExp(ip_regex)))
         self.settings_frame.lineEditServerIP.setValidator(QRegExpValidator(QRegExp(ip_regex)))
 
         self.settings_frame.lineEditClientIP.setText(self.client_ip)
-        self.settings_frame.lineEditServerIP.setText(self.server_ip)
+        #self.settings_frame.lineEditServerIP.setText(self.server_ip)
         self.settings_frame.spinBoxClientPort.setValue(self.client_port)
         self.settings_frame.spinBoxServerPort.setValue(self.server_port)
 
@@ -55,6 +57,7 @@ class NetworkSDRInterfacePlugin(SDRPlugin):
         self.settings_frame.lOpenProtoSniffer.linkActivated.connect(self.on_lopenprotosniffer_link_activated)
 
     def start_tcp_server_for_receiving(self):
+        print("Server ip:", self.server_ip)
         self.server = socketserver.TCPServer((self.server_ip, self.server_port), self.MyTCPHandler,
                                              bind_and_activate=False)
         self.server.received_bits = self.received_bits
@@ -92,25 +95,16 @@ class NetworkSDRInterfacePlugin(SDRPlugin):
         bits += "0" * ((8 - len(bits) % 8) % 8)
         return bytearray((int(bits[i:i+8], 2) for i in range(0, len(bits), 8)))
 
-    @staticmethod
-    def ip_is_valid(ip: str):
-        try:
-            socket.inet_aton(ip)
-            return True
-        except socket.error:
-            return False
-
     def on_linedit_client_ip_editing_finished(self):
         ip = self.settings_frame.lineEditClientIP.text()
-        if self.ip_is_valid(ip):
-            self.client_ip = ip
-            self.qsettings.setValue('client_ip', self.client_ip)
+        self.client_ip = ip
+        self.qsettings.setValue('client_ip', self.client_ip)
 
     def on_linedit_server_ip_editing_finished(self):
+        # Does nothing, because field is disabled
         ip = self.settings_frame.lineEditServerIP.text()
-        if self.ip_is_valid(ip):
-            self.server_ip = ip
-            self.qsettings.setValue('server_ip', self.server_ip)
+        self.server_ip = ip
+        self.qsettings.setValue('server_ip', self.server_ip)
 
     def on_spinbox_client_port_editing_finished(self):
         self.client_port = self.settings_frame.spinBoxClientPort.value()

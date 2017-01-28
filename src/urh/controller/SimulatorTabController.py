8a19f392c3e515cef7f885f4915ab07754cec4c9
splotz90 <sebastian-plotz@web.de>
2017-01-29 00:26:18 +0100

    improve code in SimulatorScene class

--- src/urh/controller/SimulatorTabController.py
+++ src/urh/controller/SimulatorTabController.py
@@ -9,7 +9,6 @@ from urh.ui.SimulatorScene import SimulatorScene
 from urh.signalprocessing.ProtocolAnalyzer import ProtocolAnalyzer
 
 from urh.controller.CompareFrameController import CompareFrameController
-from urh.signalprocessing.Message import Message
 from urh.signalprocessing.SimulatorMessage import SimulatorMessage
 from urh.signalprocessing.SimulatorProtocolLabel import SimulatorProtocolLabel
 
@@ -19,7 +18,6 @@ class SimulatorTabController(QWidget):
 
         super().__init__(parent)
 
-        self.proto_analyzer = ProtocolAnalyzer(None)
         self.project_manager = project_manager
 
         self.ui = Ui_SimulatorTab()
@@ -44,23 +42,15 @@ class SimulatorTabController(QWidget):
 
         self.create_connects(compare_frame_controller)
 
-        self.messages = []
-
     def create_connects(self, compare_frame_controller):
         self.project_manager.project_updated.connect(self.on_project_updated)
         compare_frame_controller.proto_tree_model.layoutChanged.connect(self.refresh_tree)
-        self.simulate_list_model.simulate_state_changed.connect(self.on_project_updated)
 
     def on_project_updated(self):
         self.simulate_list_model.participants = self.project_manager.participants
         self.simulate_list_model.update()
 
-        self.messages[:] = [msg for msg in self.messages if msg.source in self.project_manager.participants
-                                and msg.destination in self.project_manager.participants]
-
-        self.simulator_scene.update_participants(self.project_manager.participants)
-        self.simulator_scene.update_messages(self.messages)
-        self.simulator_scene.arrange_items()
+        self.simulator_scene.update_view()
 
     @pyqtSlot()
     def refresh_tree(self):
@@ -71,33 +61,3 @@ class SimulatorTabController(QWidget):
         self.tree_model.rootItem.clearChilds()
         self.tree_model.rootItem.addGroup()
         self.refresh_tree()
-
-    def __detect_source_destination(self, message: Message):
-        # TODO: use SRC_ADDRESS and DST_ADDRESS labels
-        participants = self.project_manager.participants
-        source = None
-        destination = None
-
-        if len(participants) < 2:
-            return (None, None)
-
-        if message.participant:
-            source = message.participant
-            destination = participants[0] if source == participants[1] else participants[1]
-        else:
-            source = participants[0]
-            destination = participants[1]
-
-        return (source, destination)
-
-    def add_protocols(self, protocols):
-        for protocol in protocols:
-            for message in protocol.messages:
-                source, destination = self.__detect_source_destination(message)
-                simulator_message = SimulatorMessage(message.message_type.name, source=source, destination=destination)
-                for label in message.message_type:
-                    simulator_message.labels.append(SimulatorProtocolLabel(label.name, label.type, label.end - label.start,
-                                                                         label.color_index, label.type))
-                self.messages.append(simulator_message)
-
-        self.on_project_updated()
--- src/urh/ui/SimulatorScene.py
+++ src/urh/ui/SimulatorScene.py
@@ -1,181 +1,189 @@
-from PyQt5.QtWidgets import QGraphicsScene, QGraphicsLineItem, QGraphicsTextItem, QGraphicsItemGroup, QGraphicsSceneDragDropEvent
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsLineItem, QGraphicsTextItem, QGraphicsItemGroup, QGraphicsSceneDragDropEvent, QGraphicsItem
 from PyQt5.QtGui import QPen, QDragEnterEvent, QDropEvent, QPolygonF, QColor, QFont
 from PyQt5.QtCore import Qt, QRectF, QSizeF, QPointF, QSizeF
 import math
 
 from urh import constants
+from urh.signalprocessing.Message import Message
 
-class LabelTextItem(QGraphicsTextItem):
-    def __init__(self, parent, color):
-        self.color = color
+class LabelItem(QGraphicsTextItem):
+    def __init__(self, text, color, parent=None):
         super().__init__(parent)
+        self.color = color
+        self.setFont(QFont("Courier", 8))
+        self.setPlainText(text)
 
     def paint(self, painter, option, widget):
         painter.setBrush(self.color)
         painter.drawRect(self.boundingRect())
         super().paint(painter, option, widget)
 
-class MessageArrowItem(QGraphicsLineItem):
-
-    def __init__(self, parent=None, source=None, destination=None):
+class ParticipantItem(QGraphicsItem):
+    def __init__(self, name, parent=None):
         super().__init__(parent)
-        self.myColor = Qt.black
-        self.setPen(QPen(self.myColor, 1, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
-        self.arrowHead = QPolygonF()
+        self.text = QGraphicsTextItem(name, self)
+        self.line = QGraphicsLineItem(self)
+        self.line.setPen(QPen(Qt.darkGray, 1, Qt.DashLine, Qt.RoundCap, Qt.RoundJoin))
+        self.update()
+
+    def update(self, x_pos=50):
+        if not self.scene():
+            return
+
+        self.text.setPos(x_pos - (self.text.boundingRect().width() / 2), 0)
+        self.line.setLine(x_pos, 30, x_pos, (len(self.scene().messages) + 1) * 50)
+        super().update()
+
+    def boundingRect(self):
+        return self.childrenBoundingRect()
+
+    def paint(self, painter, option, widget):
+        pass
+
+class MessageItem(QGraphicsItem):
+    def __init__(self, source, destination, parent=None):
+        super().__init__(parent)
+        self.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemHasNoContents)
+        self.arrow = MessageArrowItem(self)
         self.source = source
         self.destination = destination
+        self.labels = []
+
+    def labels_width(self):
+        width = sum([lbl.boundingRect().width() for lbl in self.labels])
+        width += 5 * (len(self.labels) - 1)
+        return width
+
+    def add_label(self, label: LabelItem):
+        label.setParentItem(self)
+        self.labels.append(label)
+
+    def update(self, y_pos):
+        self.arrow.setLine(self.source.line.line().x1(), y_pos, self.destination.line.line().x1(), y_pos)
+
+        start_x = min(self.source.line.line().x1(), self.destination.line.line().x1())
+        start_x += (self.arrow.line().length() - self.labels_width()) / 2
+        
+        for label in self.labels:
+            label.setPos(start_x, y_pos - (label.boundingRect().height() + 5))
+            start_x += label.boundingRect().width() + 5
+
+        super().update()
+
+    def boundingRect(self):
+        return QRectF()
+
+    def paint(self, painter, option, widget):
+        pass
+
+class MessageArrowItem(QGraphicsLineItem):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setPen(QPen(Qt.black, 1, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
 
     def boundingRect(self):
         extra = (self.pen().width() + 20) / 2.0
 
-        return QRectF(self.line().p1(), QSizeF(self.line().p2().x() - self.line().p1().x(), self.line().p2().y() - self.line().p1().y())).normalized().adjusted(-extra, -extra, extra, extra)
-
-
-    def shape(self):
-        path = super().shape()
-        path.addPolygon(self.arrowHead)
-        return path
+        return QRectF(self.line().p1(), QSizeF(self.line().p2().x() - self.line().p1().x(),
+                    self.line().p2().y() - self.line().p1().y())).normalized().adjusted(-extra, -extra, extra, extra)
 
     def paint(self, painter, option, widget):
         if self.line().length() == 0:
             return
 
         myPen = self.pen()
-        myPen.setColor(self.myColor)
+        myPen.setColor(Qt.black)
         arrowSize = 10.0
         painter.setPen(myPen)
-        painter.setBrush(self.myColor)
+        painter.setBrush(Qt.black)
 
         angle = math.acos(self.line().dx() / self.line().length())
+
         if self.line().dy() >= 0:
             angle = (math.pi * 2) - angle
 
-        arrowP1 = self.line().p2() - QPointF(math.sin(angle + math.pi / 2.5) * arrowSize, math.cos(angle + math.pi / 2.5) * arrowSize)
-        arrowP2 = self.line().p2() - QPointF(math.sin(angle + math.pi - math.pi / 2.5) * arrowSize, math.cos(angle + math.pi - math.pi / 2.5) * arrowSize)
+        arrowP1 = self.line().p2() - QPointF(math.sin(angle + math.pi / 2.5) * arrowSize,
+                    math.cos(angle + math.pi / 2.5) * arrowSize)
 
-        self.arrowHead.clear()
-        self.arrowHead.append(self.line().p2())
-        self.arrowHead.append(arrowP1)
-        self.arrowHead.append(arrowP2)
+        arrowP2 = self.line().p2() - QPointF(math.sin(angle + math.pi - math.pi / 2.5) * arrowSize,
+                    math.cos(angle + math.pi - math.pi / 2.5) * arrowSize)
+
+        arrowHead = QPolygonF()
+        arrowHead.append(self.line().p2())
+        arrowHead.append(arrowP1)
+        arrowHead.append(arrowP2)
+
         painter.drawLine(self.line())
-        painter.drawPolygon(self.arrowHead)
+        painter.drawPolygon(arrowHead)
 
 class SimulatorScene(QGraphicsScene):
     def __init__(self, parent=None, controller=None):
         super().__init__(parent)
         self.controller = controller
         self.tree_root_item = None
+
         self.participants_dict = {}
         self.participants = []
-        self.messages_dict = {}
+
         self.messages = []
 
+    def update_view(self):
+        self.update_participants(self.controller.project_manager.participants)
+
+        for msg in self.messages:
+            if msg.source not in self.participants or msg.destination not in self.participants:
+                self.removeItem(msg)
+
+        self.messages[:] = [msg for msg in self.messages
+                                if msg.source in self.participants
+                                and msg.destination in self.participants]
+
+        self.arrange_items()
+        
     def update_participants(self, participants):
         for key in list(self.participants_dict.keys()):
             if key not in participants:
-                for child in self.participants_dict[key].childItems():
-                    self.removeItem(child)
-
+                self.removeItem(self.participants_dict[key])
                 self.participants.remove(self.participants_dict[key])
-                self.destroyItemGroup(self.participants_dict[key])
                 del self.participants_dict[key]
 
         for participant in participants:
             if participant in self.participants_dict:
-                text = self.participants_dict[participant].childItems()[0]
-                text.setPlainText(participant.shortname)
+                participant_item = self.participants_dict[participant]
+                participant_item.text.setPlainText(participant.shortname)
             else:
-                text = QGraphicsTextItem(participant.shortname)
-                #text.setFont(QFont("Courier", 8))
-                line = QGraphicsLineItem()
-                line.setPen(QPen(Qt.darkGray, 1, Qt.DashLine, Qt.RoundCap, Qt.RoundJoin))
-                part_group = QGraphicsItemGroup()
-                part_group.addToGroup(text)
-                part_group.addToGroup(line)
-                self.addItem(part_group)
-                self.participants_dict[participant] = part_group
-                self.participants.append(part_group)
-
-    def update_messages(self, messages):
-        for key in list(self.messages_dict.keys()):
-            if key not in messages:
-                for child in self.messages_dict[key].childItems():
-                    self.removeItem(child)
-
-                self.messages.remove(self.messages_dict[key])
-                self.destroyItemGroup(self.messages_dict[key])
-                del self.messages_dict[key]
-
-        for message in messages:
-            if message in self.messages_dict:
-                arrow = self.messages_dict[message].childItems()[0]
-                #arrow.setPlainText(participant.shortname)
-                arrow.source = self.participants_dict[message.source]
-                arrow.destination = self.participants_dict[message.destination]
-            else:
-                arrow = MessageArrowItem()
-                arrow.source = self.participants_dict[message.source]
-                arrow.destination = self.participants_dict[message.destination]
-                msg_group = QGraphicsItemGroup()
-                msg_group.addToGroup(arrow)
-
-                for label in message.labels:
-                    label_item = LabelTextItem(None, constants.LABEL_COLORS[label.color_index])
-                    label_item.setPlainText("[Auto]")
-                   # label_item.setPos(start_x, start_y - label_item.boundingRect().height())
-                    label_item.setFont(QFont("Courier", 8))
-                    msg_group.addToGroup(label_item)
-
-                self.addItem(msg_group)
-                self.messages_dict[message] = msg_group
-                self.messages.append(msg_group)
+                participant_item = ParticipantItem(participant.shortname)
+                self.addItem(participant_item)
+                self.participants_dict[participant] = participant_item
+                self.participants.append(participant_item)
 
     def arrange_items(self):
         if len(self.participants) < 1:
             return
 
-        text = self.participants[0].childItems()[0]
-        text.setPos(50 - (text.boundingRect().width() / 2), 0)
-
-        line = self.participants[0].childItems()[1]
-        line.setLine(50, 30, 50, (len(self.messages) + 1) * 50)
+        self.participants[0].update()
 
         for i in range(1, len(self.participants)):
             curr_participant = self.participants[i]
             participants_left = self.participants[:i]
-            messages = [msg for msg in self.messages
-                    if (msg.childItems()[0].source == curr_participant and msg.childItems()[0].destination in participants_left)
-                    or (msg.childItems()[0].source in participants_left and msg.childItems()[0].destination == curr_participant)]
 
-            x_max = self.participants[i-1].childItems()[1].line().x1() + 50
+            messages = [msg for msg in self.messages
+                    if (msg.source == curr_participant and msg.destination in participants_left)
+                    or (msg.source in participants_left and msg.destination == curr_participant)]
+
+            x_max = self.participants[i - 1].line.line().x1() + 50
 
             for msg in messages:
-                width = sum([lbl.boundingRect().width() for lbl in msg.childItems()[1:]]) + 30
-                width += 5 * (len(msg.childItems()[1:]) - 1)
-                x = width + (msg.childItems()[0].source.childItems()[1].line().x1()
-                    if msg.childItems()[0].source != curr_participant else msg.childItems()[0].destination.childItems()[1].line().x1())
+                x = msg.labels_width() +  30
+                x += msg.source.line.line().x1() if msg.source != curr_participant else msg.destination.line.line().x1()
 
                 if x > x_max:
                     x_max = x
 
-            text = curr_participant.childItems()[0]
-            text.setPos(x_max - (text.boundingRect().width() / 2), 0)
-            line = curr_participant.childItems()[1].setLine(x_max, 30, x_max, (len(self.messages) + 1) * 50)
+            curr_participant.update(x_max)
 
         for i, message in enumerate(self.messages):
-            source = message.childItems()[0].source
-            destination = message.childItems()[0].destination
-            message.childItems()[0].setLine(source.childItems()[1].line().x1(), (i + 1) * 50, destination.childItems()[1].line().x1(), (i + 1) * 50)
-
-            labels_width = sum([lbl.boundingRect().width() for lbl in message.childItems()[1:]])
-            labels_width += 5 * (len(message.childItems()[1:]) - 1)
-            x = min(source.childItems()[1].line().x1(), destination.childItems()[1].line().x1())
-            start_x = x + ((message.childItems()[0].line().length() - labels_width) / 2)
-            start_y = (i + 1) * 50
-
-            for label in message.childItems()[1:]:
-                label.setPos(start_x, start_y - (label.boundingRect().height() + 5))
-                start_x += label.boundingRect().width() + 5
+            message.update((i + 1) * 50)
 
     def dragEnterEvent(self, event: QGraphicsSceneDragDropEvent):
         event.setAccepted(True)
@@ -211,6 +219,33 @@ class SimulatorScene(QGraphicsScene):
         nodes_to_add.extend([file_node for file_node in file_nodes if file_node not in nodes_to_add])
         protocols_to_add = [node.protocol for node in nodes_to_add]
 
-        self.controller.add_protocols(protocols_to_add)
+        for protocol in protocols_to_add:
+            for message in protocol.messages:
+                source, destination = self.__detect_source_destination(message)
+                simulator_message = MessageItem(self.participants_dict[source], self.participants_dict[destination])
+                for label in message.message_type:
+                    simulator_message.add_label(LabelItem(label.name, constants.LABEL_COLORS[label.color_index]))
+                self.messages.append(simulator_message)
+                self.addItem(simulator_message)
+
+        self.update_view()
 
         super().dropEvent(event)
+
+    def __detect_source_destination(self, message: Message):
+        # TODO: use SRC_ADDRESS and DST_ADDRESS labels
+        participants = self.controller.project_manager.participants
+        source = None
+        destination = None
+
+        if len(participants) < 2:
+            return (None, None)
+
+        if message.participant:
+            source = message.participant
+            destination = participants[0] if source == participants[1] else participants[1]
+        else:
+            source = participants[0]
+            destination = participants[1]
+
+        return (source, destination)

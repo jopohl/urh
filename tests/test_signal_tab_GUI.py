07f50c4a6ce4ffa60c52fadfaabc26e45e0f93bb
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-02-02 19:50:28 +0100

    add zoom test

--- tests/test_signal_tab_GUI.py
+++ tests/test_signal_tab_GUI.py
@@ -26,3 +26,19 @@ class TestSignalTabGUI(unittest.TestCase):
 
         self.form.add_signalfile(get_path_for_data_file("ask.complex"))
         self.assertEqual(self.form.signal_tab_controller.num_signals, 1)
+
+    def test_zoom(self):
+        self.form.add_signalfile(get_path_for_data_file("esaver.complex"))
+        frame = self.form.signal_tab_controller.signal_frames[0]
+        x_zoom = frame.ui.spinBoxXZoom.value()
+        self.assertEqual(x_zoom, 100)
+
+        for _ in range(10):
+            frame.ui.gvSignal.zoom(1.01)
+            self.assertGreater(frame.ui.spinBoxXZoom.value(), x_zoom)
+            x_zoom = frame.ui.spinBoxXZoom.value()
+
+        for _ in range(10):
+            frame.ui.gvSignal.zoom(0.99)
+            self.assertLess(frame.ui.spinBoxXZoom.value(), x_zoom)
+            x_zoom = frame.ui.spinBoxXZoom.value()

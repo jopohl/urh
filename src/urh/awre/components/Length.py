7bb586591f73cb3a3bebec7e041ebc5e06bbba9a
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-02-12 14:47:48 +0100

    remove code duplications

--- src/urh/awre/components/Length.py
+++ src/urh/awre/components/Length.py
@@ -7,6 +7,7 @@ from urh.awre.components.Component import Component
 from urh.signalprocessing.FieldType import FieldType
 from urh.signalprocessing.Interval import Interval
 from urh.signalprocessing.MessageType import MessageType
+from urh.signalprocessing.ProtocoLabel import ProtocolLabel
 
 
 class Length(Component):
@@ -15,8 +16,8 @@ class Length(Component):
     A length field should be a common equal range in all clusters.
     """
 
-
-    def __init__(self, fieldtypes, length_cluster, priority=2, predecessors=None, enabled=True, backend=None, messagetypes=None):
+    def __init__(self, fieldtypes, length_cluster, priority=2, predecessors=None,
+                 enabled=True, backend=None, messagetypes=None):
         super().__init__(priority, predecessors, enabled, backend, messagetypes)
 
         self.length_field_type = next((ft for ft in fieldtypes if ft.function == ft.Function.LENGTH), None)
@@ -35,7 +36,6 @@ class Length(Component):
         A simple XOR would not work as it would be very error prone.
         """
 
-
     def _py_find_field(self, messages):
         """
 
@@ -49,7 +49,7 @@ class Length(Component):
             messages_by_type[msg.message_type].append(msg)
 
         # First we get the common ranges per message length
-        common_ranges_by_length = defaultdict(lambda :defaultdict(list))
+        common_ranges_by_length = defaultdict(lambda: defaultdict(list))
         """:type: dict[urh.signalprocessing.MessageType.MessageType, dict[int, List[(int,int)]]]"""
 
         for message_type in messages_by_type.keys():
@@ -58,13 +58,14 @@ class Length(Component):
                 try:
                     cluster = self.length_cluster[vec_len]
                 except KeyError:
-                    continue # Skip message lengths that appear only once
+                    continue  # Skip message lengths that appear only once
 
                 for rng_start, rng_end in unlabeled_ranges:
                     start = 0
                     for end in np.where(cluster[rng_start:rng_end] < self.EQUAL_BIT_TRESHOLD)[0]:
                         if start < end - 1:
-                            common_ranges_by_length[message_type][vec_len].append((rng_start+start, rng_start+end-1))
+                            common_ranges_by_length[message_type][vec_len].append(
+                                (rng_start + start, rng_start + end - 1))
                         start = end + 1
 
         # Now we merge the ranges together to get our candidate ranges
@@ -96,11 +97,11 @@ class Length(Component):
         for message_type, intervals in common_intervals_by_type.items():
             assert isinstance(message_type, MessageType)
             # Exclude Synchronization (or preamble if not present) from length calculation
-            sync_lbl = next((lbl for lbl in message_type if lbl.type and lbl.type.function == FieldType.Function.SYNC), None)
+            sync_lbl = self.find_lbl_function_in(FieldType.Function.SYNC, message_type)
             if sync_lbl:
                 sync_len = self.__nbits2bytes(sync_lbl.end)
             else:
-                preamble_lbl = next((lbl for lbl in message_type if lbl.type and lbl.type.function == FieldType.Function.PREAMBLE), None)
+                preamble_lbl = self.find_lbl_function_in(FieldType.Function.PREAMBLE, message_type)
                 sync_len = self.__nbits2bytes(preamble_lbl.end) if preamble_lbl is not None else 0
 
             scores = defaultdict(int)
@@ -123,7 +124,8 @@ class Length(Component):
 
             try:
                 start, end = max(scores, key=scores.__getitem__)
-                if not any((lbl.type.function == FieldType.Function.LENGTH or lbl.name == "Length") and lbl.auto_created for lbl in message_type):
+                if not any((lbl.type.function == FieldType.Function.LENGTH or lbl.name == "Length") and lbl.auto_created
+                           for lbl in message_type):
                     message_type.add_protocol_label(start=start, end=end - 1, name=self.length_field_name,
                                                     auto_created=True, type=self.length_field_type)
             except ValueError:
@@ -131,3 +133,7 @@ class Length(Component):
 
     def __nbits2bytes(self, nbits):
         return int(math.ceil(nbits / 8))
+
+    @staticmethod
+    def find_lbl_function_in(function: FieldType.Function, message_type: MessageType) -> ProtocolLabel:
+        return next((lbl for lbl in message_type if lbl.type and lbl.type.function == function), None)
--- src/urh/ui/LegendScene.py
+++ src/urh/ui/LegendScene.py
@@ -9,31 +9,27 @@ class LegendScene(QGraphicsScene):
         self.ones_arrow = None
         self.zeros_arrow = None
 
-    def draw_one_zero_arrows(self, ymid):
+    def draw_one_zero_arrows(self, y_mid):
         y = self.sceneRect().y()
         h = self.sceneRect().height()
-        if ymid < y:
-            ymid = y
-        elif ymid > y + h:
-            ymid = y + h
+        if y_mid < y:
+            y_mid = y
+        elif y_mid > y + h:
+            y_mid = y + h
 
         w_view = self.sceneRect().width()
 
-        if self.zeros_arrow is None:
-            self.zeros_arrow = LabeledArrow(w_view / 2, y + h / 2 + ymid, w_view / 2, y + h, 0)
-            self.addItem(self.zeros_arrow)
-        else:
+        if self.zeros_arrow is not None:
             self.removeItem(self.zeros_arrow)
-            self.zeros_arrow = LabeledArrow(w_view / 2, y + h / 2 + ymid, w_view / 2, y + h, 0)
-            self.addItem(self.zeros_arrow)
 
-        if self.ones_arrow is None:
-            self.ones_arrow = LabeledArrow(w_view / 2, y, w_view / 2, y + h / 2 + ymid, 1)
-            self.addItem(self.ones_arrow)
-        else:
+        self.zeros_arrow = LabeledArrow(w_view / 2, y + h / 2 + y_mid, w_view / 2, y + h, 0)
+        self.addItem(self.zeros_arrow)
+
+        if self.ones_arrow is not None:
             self.removeItem(self.ones_arrow)
-            self.ones_arrow = LabeledArrow(w_view / 2, y, w_view / 2, y + h / 2 + ymid, 1)
-            self.addItem(self.ones_arrow)
+
+        self.ones_arrow = LabeledArrow(w_view / 2, y, w_view / 2, y + h / 2 + y_mid, 1)
+        self.addItem(self.ones_arrow)
 
     def clear(self):
         self.zeros_arrow = None

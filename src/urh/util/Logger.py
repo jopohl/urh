b0c84557e12e87a281c17417bb142fb2a6541884
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-02-13 07:23:24 +0100

    use dict for color definition

--- src/urh/util/Logger.py
+++ src/urh/util/Logger.py
@@ -2,9 +2,15 @@ import logging
 from urh.constants import color
 
 logging.basicConfig(level=logging.WARNING, format='[%(levelname)s] %(message)s')
-logging.addLevelName(logging.WARNING, "{0}{1}{2}".format(color.YELLOW+color.BOLD, logging.getLevelName(logging.WARNING), color.END))
-logging.addLevelName(logging.ERROR, "{0}{1}{2}".format(color.RED+color.BOLD, logging.getLevelName(logging.ERROR), color.END))
-logging.addLevelName(logging.CRITICAL, "{0}{1}{2}".format(color.RED+color.BOLD, logging.getLevelName(logging.CRITICAL), color.END))
+
+logging_colors_per_level = {
+    logging.WARNING: color.YELLOW,
+    logging.ERROR: color.RED,
+    logging.CRITICAL: color.RED
+}
+
+for level, level_color in logging_colors_per_level.items():
+    logging.addLevelName(level, "{0}{1}{2}".format(level_color+color.BOLD, logging.getLevelName(level), color.END))
 
 logger = logging.getLogger("urh")
 logger.setLevel(logging.DEBUG)

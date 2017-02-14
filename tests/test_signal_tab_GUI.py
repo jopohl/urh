2a2c410b7a9763581cf54439a9f6b389f5ada436
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-02-14 23:14:05 +0100

    enhance unittests for signal frame

--- tests/test_signal_tab_GUI.py
+++ tests/test_signal_tab_GUI.py
@@ -42,3 +42,38 @@ class TestSignalTabGUI(unittest.TestCase):
             frame.ui.gvSignal.zoom(0.99)
             self.assertLess(frame.ui.spinBoxXZoom.value(), x_zoom)
             x_zoom = frame.ui.spinBoxXZoom.value()
+
+        samples_in_view = frame.ui.lSamplesInView.text()
+        frame.ui.spinBoxXZoom.setValue(frame.ui.spinBoxXZoom.value() + 400)
+        self.assertNotEqual(samples_in_view, frame.ui.lSamplesInView.text())
+
+    def test_load_proto(self):
+        self.form.add_files([get_path_for_data_file("protocol.proto")])
+        self.assertEqual(self.form.signal_tab_controller.signal_frames[0].ui.lSignalTyp.text(), "Protocol (*.proto)")
+
+    def test_graphic_view_selection(self):
+        self.form.add_signalfile(get_path_for_data_file("esaver.complex"))
+        frame = self.form.signal_tab_controller.signal_frames[0]
+
+        frame.ui.gvSignal.selection_area.start = 0
+        frame.ui.gvSignal.selection_area.end = 4000
+        frame.ui.gvSignal.sel_area_start_end_changed.emit(0, 4000)
+
+        self.assertEqual(frame.ui.lNumSelectedSamples.text(), "4000")
+
+        noise_val = frame.ui.spinBoxNoiseTreshold.value()
+        frame.ui.gvSignal.set_noise_clicked.emit()
+        self.assertNotEqual(noise_val, frame.ui.spinBoxNoiseTreshold.value())
+
+        frame.ui.spinBoxSelectionStart.setValue(300)
+        self.assertEqual(frame.ui.gvSignal.selection_area.start, 300)
+        frame.ui.spinBoxSelectionEnd.setValue(6000)
+        self.assertEqual(frame.ui.gvSignal.selection_area.end, 6000)
+
+    def test_show_hide_start_end(self):
+        self.form.add_signalfile(get_path_for_data_file("esaver.complex"))
+        frame = self.form.signal_tab_controller.signal_frames[0]
+
+        self.assertEqual(frame.ui.btnShowHideStartEnd.text(), "+")
+        frame.ui.btnShowHideStartEnd.click()
+        self.assertEqual(frame.ui.btnShowHideStartEnd.text(), "-")

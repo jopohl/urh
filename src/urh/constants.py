89330a2225e76691eb17880e249a22c9347498e5
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-02-15 21:33:14 +0100

    use selection color for send indicator

--- src/urh/constants.py
+++ src/urh/constants.py
@@ -33,7 +33,7 @@ BGCOLOR = QColor.fromRgb(55, 53, 53)
 AXISCOLOR = QColor.fromRgb(200, 200, 200, 100)
 ARROWCOLOR = QColor.fromRgb(204, 120, 50)
 
-SEND_INDICATOR_COLOR = QColor(255, 0, 0, 33)
+SEND_INDICATOR_COLOR = QColor("darkblue")   # overwritten by system color (bin/urh)
 
 # ROI-SELECTION COLORS
 SELECTION_COLOR = QColor("darkblue") # overwritten by system color (bin/urh)

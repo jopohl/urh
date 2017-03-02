8d2a24b8d46300ab9f5c2de2757cc62cee768421
andynoack <andreas.noack@fh-stralsund.de>
2017-03-02 11:59:50 +0100

    auto .fuzz ending for fuzz profiles

--- src/urh/controller/GeneratorTabController.py
+++ src/urh/controller/GeneratorTabController.py
@@ -504,7 +504,7 @@ class GeneratorTabController(QWidget):
 
     @pyqtSlot()
     def on_btn_save_clicked(self):
-        filename = FileOperator.get_save_file_name("profile", parent=self, caption="Save fuzz profile")
+        filename = FileOperator.get_save_file_name("profile.fuzz", parent=self, caption="Save fuzz profile")
         if filename:
             self.table_model.protocol.to_xml_file(filename)
 

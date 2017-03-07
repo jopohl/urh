311ce261ba8473383e33e8730ffbdf1457b441dd
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-03-07 14:50:34 +0100

    add usrp params

--- src/urh/dev/config.py
+++ src/urh/dev/config.py
@@ -7,10 +7,20 @@ DEVICE_CONFIG = OrderedDict()
 
 # https://github.com/mossmann/hackrf/wiki/HackRF-One#features
 DEVICE_CONFIG["HackRF"] = {
-    "frequency": (10**6, 6*10**9),
+    "center_freq": (10**6, 6*10**9),
     "sample_rate": (2*10**6, 20*10**6),
     "bandwidth": (2*10**6, 20*10**6),
     "rf_gain": [0, 14],
     "if_gain": list(range(0, 41, 8)),
     "baseband_gain": list(range(0, 48))  # only available in RX
+}
+
+# https://kb.ettus.com/About_USRP_Bandwidths_and_Sampling_Rates
+DEVICE_CONFIG["USRP"] = {
+    "center_freq": (0, 6*10**9),
+    "sample_rate": (1, 200*10**6),
+    "bandwidth": (1, 120*10**6),
+    "device_args": "",
+    "rf_gain": (0, 100),
+    "antenna": [0, 1],
 }

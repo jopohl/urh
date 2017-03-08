5c80ab8587a3c30af6c1b541c51bc6657885652b
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-03-08 08:20:28 +0100

    use namedtuple for range definition

--- src/urh/dev/config.py
+++ src/urh/dev/config.py
@@ -1,4 +1,4 @@
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 
 import copy
 
@@ -6,38 +6,39 @@ from urh.plugins.NetworkSDRInterface.NetworkSDRInterfacePlugin import NetworkSDR
 
 DEVICE_CONFIG = OrderedDict()
 
-# List = supported values
-# Tuple = start/end for range
+dev_range = namedtuple("dev_range", ["start", "stop", "step"])
 
+M = 10**6
+G = 10 ** 9
 # https://github.com/mossmann/hackrf/wiki/HackRF-One#features
 DEVICE_CONFIG["HackRF"] = {
-    "center_freq": (10**6, 6*10**9),
-    "sample_rate": (2*10**6, 20*10**6),
-    "bandwidth": (2*10**6, 20*10**6),
-    "rf_gain": [0, 14],
-    "if_gain": list(range(0, 41, 8)),
-    "baseband_gain": list(range(0, 48))  # only available in RX
+    "center_freq": dev_range(start=1*M, stop=6 * G, step=1),
+    "sample_rate": dev_range(start=2*M, stop=20 * M, step=1),
+    "bandwidth": dev_range(start=2 * M, stop=20 * M, step=1),
+    "rf_gain": dev_range(start=0, stop=14, step=14),
+    "if_gain": dev_range(start=0, stop=40, step=8),
+    "baseband_gain": dev_range(start=0, stop=47, step=1)  # only available in RX
 }
 
 # https://kb.ettus.com/About_USRP_Bandwidths_and_Sampling_Rates
 DEVICE_CONFIG["USRP"] = {
-    "center_freq": (0, 6*10**9),
-    "sample_rate": (1, 200*10**6),
-    "bandwidth": (1, 120*10**6),
+    "center_freq": dev_range(start=0, stop=6 * G, step=1),
+    "sample_rate": dev_range(start=1, stop=200 * M, step=1),
+    "bandwidth": dev_range(start=1, stop=120 * M, step=1),
     "device_args": "",
     "ip": "",
-    "rf_gain": (0, 100),
-    "antenna": [0, 1],
+    "rf_gain": dev_range(start=0, stop=100, step=1),
+    "antenna": dev_range(start=0, stop=1, step=1)
 }
 
 # http://osmocom.org/projects/sdr/wiki/rtl-sdr
 DEVICE_CONFIG["RTL-SDR"] = {
-    "center_freq": (22*10**6, 2200*10**6),
-    "sample_rate": (1, int(3.2*10**6)),
-    "bandwidth": (1, int(3.2*10**6)),
-    "rf_gain": (0, 50),  # CAUTION: API is *10 so e.g. 1 needs to be given as 10 to API
-    "direct_sampling": [0, 1, 2],  # 0 means disabled, 1 I-ADC input enabled, 2 Q-ADC input enabled
-    "freq_correction": (-1*10**3, 1*10**3)
+    "center_freq": dev_range(start=22 * M, stop=2200 * M, step=1),
+    "sample_rate": dev_range(start=1, stop=int(3.2 * M), step=1),
+    "bandwidth": dev_range(start=1, stop=int(3.2 * M), step=1),
+    "rf_gain": dev_range(start=0, stop=50, step=1),  # CAUTION: API is *10 so e.g. 1 needs to be given as 10 to API
+    "direct_sampling": dev_range(start=0, stop=2, step=1),  # 0 disabled, 1 I-ADC input enabled, 2 Q-ADC input enabled
+    "freq_correction": dev_range(start=-1 * 10 ** 3, stop=1 * 10 ** 3, step=1)
 }
 
 DEVICE_CONFIG["RTL-TCP"] = copy.deepcopy(DEVICE_CONFIG["RTL-SDR"])
@@ -47,8 +48,8 @@ DEVICE_CONFIG["RTL-TCP"]["port"] = ""
 DEVICE_CONFIG[NetworkSDRInterfacePlugin.NETWORK_SDR_NAME] = {}
 
 DEVICE_CONFIG["Fallback"] = {
-    "center_freq": (10**6, 6*10**9),
-    "sample_rate": (2*10**6, 20*10**6),
-    "bandwidth": (2*10**6, 20*10**6),
-    "rf_gain": (0, 50),
+    "center_freq": dev_range(start=1*M, stop=6 * G, step=1),
+    "sample_rate": dev_range(start=2 * M, stop=20 * M, step=1),
+    "bandwidth": dev_range(start=2 * M, stop=20 * M, step=1),
+    "rf_gain": dev_range(start=0, stop=50, step=1),
 }

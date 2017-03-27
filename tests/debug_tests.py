83c6499633ffcd765c27be31512d25da111a4527
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-03-27 19:01:35 +0200

    cleanup

--- tests/debug_tests.py
+++ tests/debug_tests.py
@@ -5,9 +5,6 @@ import sys
 import time
 
 RUNS = 2000
-LOG_SIZE_MB = 1
-
-
 os.system("mkdir -p /tmp/tests")
 
 streak = 0
@@ -15,21 +12,20 @@ longest_streak = 0
 
 for i in range(RUNS):
     try:
-        time.sleep(0.1)
         filename = "/tmp/tests/" + str(datetime.datetime.now()).replace(" ", "-")
         t = time.time()
         completed = subprocess.run("pytest -s -v ../tests", shell=True, stdout=subprocess.PIPE,
                                    stderr=subprocess.STDOUT)
-
+        duration = time.time() - t
         if completed.returncode == 0:
             streak += 1
             longest_streak = max(streak, longest_streak)
-            print("#{} was successful [{:.2f}s] (Streak: {}/{})".format(i + 1, time.time() - t, streak, longest_streak))
+            print("#{} was successful [{:.2f}s] (Streak: {}/{})".format(i + 1, duration, streak, longest_streak))
         else:
             streak = 0
-            print("#{} failed [{:.2f}s]".format(i + 1, time.time() - t))
+            print("#{} failed [{:.2f}s]".format(i + 1, duration))
             with open(filename, "wb") as f:
-                f.write(completed.stdout[-LOG_SIZE_MB * 1024 * 1024:])
+                f.write(completed.stdout)
             print("Written output to file {}".format(filename))
     except KeyboardInterrupt:
         sys.exit(1)

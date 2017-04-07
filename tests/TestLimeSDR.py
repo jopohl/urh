d5c72cf722628c02b066cb251ccd286aff07256e
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-04-07 20:12:50 +0200

    adapt tests to new cython wrapper flags

--- tests/TestLimeSDR.py
+++ tests/TestLimeSDR.py
@@ -24,42 +24,45 @@ class TestLimeSDR(unittest.TestCase):
         print("Is Open 0:", limesdr.is_open(0))
         print("Is Open 1:", limesdr.is_open(1))
         print("Init", limesdr.init())
-        print("Num Channels TX:", limesdr.get_num_channels(True))
-        print("Num Channels RX:", limesdr.get_num_channels(False))
-        print("Enable RX Channel 0:", limesdr.enable_channel(dir_tx=False, chan=0, enabled=True))
+        limesdr.IS_TX = True
+        print("Num Channels TX:", limesdr.get_num_channels())
+        limesdr.IS_TX = False
+        print("Num Channels RX:", limesdr.get_num_channels())
+        limesdr.CHANNEL = 0
+        print("Enable RX Channel 0:", limesdr.enable_channel(True))
 
-        print("RX Sample Rate Range:", limesdr.get_sample_rate_range(False))
-        print("RX Channel 0 Sample Rate:", limesdr.get_sample_rate(dir_tx=False, chan=0))
-        print("Set Sample Rate:", limesdr.set_sample_rate(2e6, 0))
-        print("RX Channel 0 Sample Rate:", limesdr.get_sample_rate(dir_tx=False, chan=0))
+        print("RX Sample Rate Range:", limesdr.get_sample_rate_range())
+        print("RX Channel 0 Sample Rate:", limesdr.get_sample_rate())
+        print("Set Sample Rate:", limesdr.set_sample_rate(2e6))
+        print("RX Channel 0 Sample Rate:", limesdr.get_sample_rate())
 
-        print("RX Frequency Range:", limesdr.get_center_frequency_range(False))
-        print("RX 0 center freq:", limesdr.get_center_frequency(False, 0))
-        print("RX 0 set center freq:", limesdr.set_center_frequency(False, 0, 433.92e6))
-        print("RX 0 center freq:", limesdr.get_center_frequency(False, 0))
+        print("RX Frequency Range:", limesdr.get_center_frequency_range())
+        print("RX 0 center freq:", limesdr.get_center_frequency())
+        print("RX 0 set center freq:", limesdr.set_center_frequency(433.92e6))
+        print("RX 0 center freq:", limesdr.get_center_frequency())
 
-        print("RX 0 gain", limesdr.get_normalized_gain(False, 0))
-        print("RX 0 set gain", limesdr.set_normalized_gain(False, 0, 0.5))
-        print("RX 0 gain", limesdr.get_normalized_gain(False, 0))
+        print("RX 0 gain", limesdr.get_normalized_gain())
+        print("RX 0 set gain", limesdr.set_normalized_gain(0.5))
+        print("RX 0 gain", limesdr.get_normalized_gain())
 
-        print("RX Bandwidth Range", limesdr.get_lpf_bandwidth_range(False))
-        print("RX 0 Bandwidth", limesdr.get_lpf_bandwidth(False, 0))
-        print("RX 0 set Bandwidth", limesdr.set_lpf_bandwidth(False, 0, 20e6))
-        print("RX 0 Bandwidth", limesdr.get_lpf_bandwidth(False, 0))
+        print("RX Bandwidth Range", limesdr.get_lpf_bandwidth_range())
+        print("RX 0 Bandwidth", limesdr.get_lpf_bandwidth())
+        print("RX 0 set Bandwidth", limesdr.set_lpf_bandwidth(20e6))
+        print("RX 0 Bandwidth", limesdr.get_lpf_bandwidth())
 
-        print("RX 0 calibrate:", limesdr.calibrate(False, 0, 20e6))
+        print("RX 0 calibrate:", limesdr.calibrate(20e6))
 
-        antenna_list = limesdr.get_antenna_list(False, 0)
+        antenna_list = limesdr.get_antenna_list()
         print("RX 0 antenna list", antenna_list)
-        print("RX 0 current antenna", limesdr.get_antenna(False, 0), antenna_list[limesdr.get_antenna(False, 0)])
-        print("RX 0 current antenna BW", limesdr.get_antenna_bw(False, 0, limesdr.get_antenna(False, 0)))
+        print("RX 0 current antenna", limesdr.get_antenna(), antenna_list[limesdr.get_antenna()])
+        print("RX 0 current antenna BW", limesdr.get_antenna_bw(limesdr.get_antenna()))
 
         print("Chip Temperature", limesdr.get_chip_temperature())
 
         parent_conn, child_conn = Pipe()
 
         for _ in range(2):
-            print("Setup stream", limesdr.setup_stream(False, 0, 1000))
+            print("Setup stream", limesdr.setup_stream(1000))
             print("Start stream", limesdr.start_stream())
             limesdr.recv_stream(child_conn, 1000, 100)
             print("Stop stream", limesdr.stop_stream())

782afcd43c52898940fd0d703f20e8c610c0e850
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-04-10 18:50:09 +0200

    set send timeout to 500 ms

--- src/urh/dev/native/LimeSDR.py
+++ src/urh/dev/native/LimeSDR.py
@@ -15,7 +15,7 @@ class LimeSDR(Device):
     SEND_FIFO_SIZE = 5 * SEND_SAMPLES
 
     LIME_TIMEOUT_RECEIVE_MS = 10
-    LIME_TIMEOUT_SEND_MS = 1000
+    LIME_TIMEOUT_SEND_MS = 500
 
     BYTES_PER_SAMPLE = 8  # We use dataFmt_t.LMS_FMT_F32 so we have 32 bit floats for I and Q
     DEVICE_LIB = limesdr

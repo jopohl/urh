c67295403fd0fe17991b41b24ade46da9cf31ed0
jopohl <Johannes.Pohl90@gmail.com>
2017-04-13 21:56:48 +0200

    make limetest runable from command line

--- tests/TestLimeSDR.py
+++ tests/TestLimeSDR.py
@@ -3,6 +3,11 @@ import sys
 
 import os
 from multiprocessing import Pipe
+f = os.readlink(__file__) if os.path.islink(__file__) else __file__
+path = os.path.realpath(os.path.join(f, "..", "..", "src"))
+
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 import numpy as np
 

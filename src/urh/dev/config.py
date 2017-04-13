b085c10f240c95d8e5a8e82eb7c6222e8fc6ada3
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-04-14 00:27:35 +0200

    increase minimum bandwidth and samplerate to 2M

--- src/urh/dev/config.py
+++ src/urh/dev/config.py
@@ -47,8 +47,8 @@ DEVICE_CONFIG["USRP"] = {
 # https://myriadrf.org/projects/limesdr/
 DEVICE_CONFIG["LimeSDR"] = {
     "center_freq": dev_range(start=100 * K, stop=int(3.8 * G), step=1),
-    "sample_rate": dev_range(start=int(1.4 * M), stop=30 * M, step=1),
-    "bandwidth": dev_range(start=int(1.4 * M), stop=130 * M, step=1),
+    "sample_rate": dev_range(start=2*M, stop=30 * M, step=1),
+    "bandwidth": dev_range(start=2*M, stop=130 * M, step=1),
     "rx_rf_gain": list(range(0, 101)),  # Normalized Gain 0-100%
     "tx_rf_gain": list(range(0, 101)),  # Normalized Gain 0-100%
     "rx_channel": ["RX1", "RX2"],

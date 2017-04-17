c112b89195b3605d1d1eacf44cf882061f1cf18c
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-04-17 16:19:32 +0200

    prevent divide by zero error

--- src/urh/controller/SendRecvDialogController.py
+++ src/urh/controller/SendRecvDialogController.py
@@ -568,7 +568,7 @@ class SendRecvDialogController(QDialog):
         self.ui.lSignalSize.setText(locale.format_string("%.2f", (8 * self.device.current_index) / (1024 ** 2)))
         self.ui.lTime.setText(locale.format_string("%.2f", self.device.current_index / self.device.sample_rate))
 
-        if self.is_rx and self.device.data is not None:
+        if self.is_rx and self.device.data is not None and len(self.device.data) > 0:
             self.ui.labelReceiveBufferFull.setText("{0}%".format(int(100 * self.device.current_index /
                                                                      len(self.device.data))))
 

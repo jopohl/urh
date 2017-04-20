3ee3735c81bf463ff6201c52319995865e9f6589
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-04-20 09:36:57 +0200

    use realpath for #244

--- src/urh/main.py
+++ src/urh/main.py
@@ -47,7 +47,7 @@ def main():
     urh_exe = sys.executable if hasattr(sys, 'frozen') else sys.argv[0]
     urh_exe = os.readlink(urh_exe) if os.path.islink(urh_exe) else urh_exe
 
-    urh_dir = os.path.join(os.path.dirname(urh_exe), "..", "..")
+    urh_dir = os.path.join(os.path.dirname(os.path.realpath(urh_exe)), "..", "..")
     prefix = os.path.abspath(os.path.normpath(urh_dir))
 
     src_dir = os.path.join(prefix, "src")

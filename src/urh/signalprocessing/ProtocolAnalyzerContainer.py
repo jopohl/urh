10ee35066ea8483adecaf8c2eb1a27c50a6b1771
jopohl <Johannes.Pohl90@gmail.com>
2017-04-28 22:53:22 +0200

    shallow copy of message type

--- src/urh/signalprocessing/ProtocolAnalyzerContainer.py
+++ src/urh/signalprocessing/ProtocolAnalyzerContainer.py
@@ -20,6 +20,7 @@ class FuzzMode(Enum):
     concurrent = 1
     exhaustive = 2
 
+
 class ProtocolAnalyzerContainer(ProtocolAnalyzer):
     """
     A container to manage several ProtocolAnalyzers.
@@ -37,7 +38,7 @@ class ProtocolAnalyzerContainer(ProtocolAnalyzer):
         self.fuzz_pause = 10000
 
         self.__group = ProtocolGroup("GeneratorGroup")
-        self.__group.add_protocol_item(ProtocolTreeItem(self, None)) # Warning: parent is None
+        self.__group.add_protocol_item(ProtocolTreeItem(self, None))  # Warning: parent is None
 
     @property
     def protocol_labels(self):
@@ -50,17 +51,11 @@ class ProtocolAnalyzerContainer(ProtocolAnalyzer):
         return any(len(msg.active_fuzzing_labels) > 1 for msg in self.messages)
 
     def insert_protocol_analyzer(self, index: int, proto_analyzer: ProtocolAnalyzer):
-        t = time.time()
-        copy_time = 0
         for msg in reversed(proto_analyzer.messages):
-            ct = time.time()
-            mt = copy.deepcopy(msg.message_type)
-            copy_time += time.time()-ct
             self.messages.insert(index, Message(plain_bits=msg.decoded_bits, pause=msg.pause,
-                            message_type=mt,
-                            rssi=msg.rssi, modulator_indx=0, decoder=msg.decoder, bit_len=msg.bit_len, participant=msg.participant))
-        print("Total insert time", time.time()-t)
-        print("Copy time", copy_time)
+                                                message_type=copy.copy(msg.message_type),
+                                                rssi=msg.rssi, modulator_indx=0, decoder=msg.decoder,
+                                                bit_len=msg.bit_len, participant=msg.participant))
 
         if len(self.pauses) > 0:
             self.fuzz_pause = self.pauses[0]
@@ -85,7 +80,8 @@ class ProtocolAnalyzerContainer(ProtocolAnalyzer):
             elif mode == FuzzMode.concurrent:
                 num_values = numpy.max([len(l.fuzz_values) for l in labels]) if labels else 0
                 f = lambda index, label: index if index < len(label.fuzz_values) else 0
-                combinations = [[(l.start, l.end, l.fuzz_values[f(j, l)]) for l in labels] for j in range(1, num_values)]
+                combinations = [[(l.start, l.end, l.fuzz_values[f(j, l)]) for l in labels] for j in
+                                range(1, num_values)]
             elif mode == FuzzMode.exhaustive:
                 pool = [[(l.start, l.end, fv) for fv in l.fuzz_values[1:]] for l in labels]
                 combinations = itertools.product(*pool) if labels else []
@@ -139,8 +135,10 @@ class ProtocolAnalyzerContainer(ProtocolAnalyzer):
     def set_decoder_for_messages(self, decoder, messages=None):
         raise NotImplementedError("Encoding cant be set in Generator!")
 
-    def to_xml_file(self, filename: str, decoders=None, participants=None, tag_name="fuzz_profile", include_message_types=True, write_bits=True):
-        super().to_xml_file(filename=filename, decoders=None, participants=participants, tag_name=tag_name, include_message_types=include_message_types, write_bits=write_bits)
+    def to_xml_file(self, filename: str, decoders=None, participants=None, tag_name="fuzz_profile",
+                    include_message_types=True, write_bits=True):
+        super().to_xml_file(filename=filename, decoders=None, participants=participants, tag_name=tag_name,
+                            include_message_types=include_message_types, write_bits=write_bits)
 
     def from_xml_file(self, filename: str, read_bits=True):
         super().from_xml_file(filename=filename, read_bits=read_bits)

1a65f99adc55ecd3815fa5a832aee03881bb2390
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-05-16 09:43:52 +0200

    adapt uhd cython wrapper and unittest to integration

--- src/urh/dev/native/lib/usrp.pyx
+++ src/urh/dev/native/lib/usrp.pyx
@@ -1,12 +1,18 @@
 from cusrp cimport *
-# noinspection PyUnresolvedReferences
 import numpy as np
+# noinspection PyUnresolvedReferences
 cimport numpy as np
-from libc.stdlib cimport malloc, free
+from libc.stdlib cimport malloc
+# noinspection PyUnresolvedReferences
+from cython.view cimport array as cvarray  # needed for converting of malloc array to python array
+
+
+from urh.util.Logger import logger
 
 np.import_array()
 
 cdef uhd_usrp_handle _c_device
+cdef uhd_rx_streamer_handle rx_streamer_handle
 
 cpdef find_devices(device_args):
     """
@@ -34,18 +40,8 @@ cpdef uhd_error open(str device_args):
 cpdef uhd_error close():
     return uhd_usrp_free(&_c_device)
 
-
-def receive(device_args):
-    py_byte_string = device_args.encode('UTF-8')
-    cdef char* dev_args = py_byte_string
-
-    cdef uhd_usrp_handle usrp_handle
-    cdef uhd_rx_streamer_handle rx_streamer_handle
-    uhd_rx_streamer_make(&rx_streamer_handle)
-    print("Made rx_streame handler")
-
+cpdef uhd_error setup_stream(bool is_tx):
     cdef uhd_stream_args_t stream_args
-
     # https://files.ettus.com/manual/structuhd_1_1stream__args__t.html
     byte_string_cpu_format = "fc32".encode("UTF-8")
     cdef char* cpu_format = byte_string_cpu_format
@@ -60,53 +56,51 @@ def receive(device_args):
     stream_args.channel_list = &channel
     stream_args.n_channels = 1
 
+    cdef size_t num_channels = 0
+
+    if not is_tx:
+        uhd_rx_streamer_make(&rx_streamer_handle)
+        uhd_usrp_get_rx_stream(_c_device, &stream_args, rx_streamer_handle)
+
+
+        uhd_rx_streamer_num_channels(rx_streamer_handle, &num_channels)
+        print("Num channels", num_channels)
+
+
+    else:
+        raise NotImplementedError("ToDo")
+
+cpdef uhd_error destroy_stream(bool is_tx):
+    if not is_tx:
+        return  uhd_rx_streamer_free(&rx_streamer_handle)
+    else:
+        raise NotImplementedError("ToDo")
+
+cpdef uhd_error recv_stream(connection, size_t num_samples):
     cdef uhd_stream_cmd_t stream_cmd
     stream_cmd.stream_mode = uhd_stream_mode_t.UHD_STREAM_MODE_NUM_SAMPS_AND_DONE
-    stream_cmd.num_samps = 363
+    stream_cmd.num_samps = num_samples
     stream_cmd.stream_now = 1
 
-    uhd_usrp_make(&usrp_handle, dev_args)
-    print("Called usrp_make")
-
-    uhd_usrp_get_rx_stream(usrp_handle, &stream_args, rx_streamer_handle)
-    print("Called get_rx_stream")
-
-    cdef size_t num_channels = 0
-    uhd_rx_streamer_num_channels(rx_streamer_handle, &num_channels)
-    print("Num channels", num_channels)
-
-    cdef size_t max_num_samps_out
-    uhd_rx_streamer_max_num_samps(rx_streamer_handle, &max_num_samps_out)
-    print("Max num samps:", max_num_samps_out)
-
-
-
-    cdef float* buff = <float *>malloc(max_num_samps_out * 2 * sizeof(float))
-    buff[0] = 42
+    cdef float* buff = <float *>malloc(num_samples * 2 * sizeof(float))
     cdef void ** buffs = <void **> &buff
-
-    #cdef np.complex64_t[:] buffer = np.empty(max_num_samps_out, dtype=np.complex64)
-    #cdef void ** buffs = <void **> malloc(buffer.shape[0]*sizeof(void *))
-    #buffs[0] = <void *>buffer
 
     cdef uhd_rx_metadata_handle metadata_handle
     uhd_rx_metadata_make(&metadata_handle)
 
     cdef size_t items_received
 
-    print("Buffer 0 before RX", buff[0])
-
     uhd_rx_streamer_issue_stream_cmd(rx_streamer_handle, &stream_cmd)
-    uhd_rx_streamer_recv(rx_streamer_handle, buffs, max_num_samps_out, &metadata_handle, 1, 0, &items_received)
+    uhd_rx_streamer_recv(rx_streamer_handle, buffs, num_samples, &metadata_handle, 1, 0, &items_received)
+
+    uhd_rx_metadata_free(&metadata_handle)
 
     print("Received items:", items_received)
+    if items_received > 0:
+        connection.send_bytes(<float[:2*items_received]>buff)
+    else:
+        logger.warning("USRP: Failed to receive stream")
 
-    print("Buffer 0 after RX", buff[0])
 
-    uhd_rx_streamer_free(&rx_streamer_handle)
-    print("Freed rx streamer handler")
-    uhd_rx_metadata_free(&metadata_handle)
-    print("Freed metadata handle")
-    uhd_usrp_free(&usrp_handle)
-    print("Freed usrp handle")
+
 
--- tests/TestUSRP.py
+++ tests/TestUSRP.py
@@ -1,3 +1,5 @@
+from multiprocessing.connection import Pipe
+
 from urh.dev.native.lib import usrp
 import unittest
 
@@ -10,13 +12,21 @@ class TestUSRP(unittest.TestCase):
         return_code = usrp.open("addr=192.168.10.2")
         print("open", return_code)
 
+        usrp.setup_stream(False)
+        print("Made rx_streame handler")
 
+        parent_conn, child_conn = Pipe()
+        for _ in range(3):
+            usrp.recv_stream(child_conn, 8192)
+            print(parent_conn.recv_bytes())
 
+        usrp.destroy_stream(False)
+        print("Freed rx streamer handler")
 
         return_code = usrp.close()
         print("close", return_code)
 
-        #usrp.receive("addr=192.168.10.2")
+
         #self.assertTrue(True)
 
 if __name__ == '__main__':

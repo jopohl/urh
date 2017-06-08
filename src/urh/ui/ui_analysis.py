5a7901ca1906c258c7745a3c8f26a88002123396
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-06-08 17:40:50 +0200

    restore table header

--- src/urh/ui/ui_analysis.py
+++ src/urh/ui/ui_analysis.py
@@ -481,7 +481,7 @@ class Ui_TabAnalysis(object):
         self.tblLabelValues.setAlternatingRowColors(True)
         self.tblLabelValues.setShowGrid(False)
         self.tblLabelValues.setObjectName("tblLabelValues")
-        self.tblLabelValues.horizontalHeader().setVisible(False)
+        self.tblLabelValues.horizontalHeader().setVisible(True)
         self.tblLabelValues.horizontalHeader().setCascadingSectionResizes(False)
         self.tblLabelValues.horizontalHeader().setDefaultSectionSize(150)
         self.tblLabelValues.horizontalHeader().setStretchLastSection(True)

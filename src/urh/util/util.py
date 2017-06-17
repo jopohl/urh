9f5116c53c95247f480ceccfb5802ff53820a3c7
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-06-17 15:01:04 +0200

    show expected CRC in value table

--- src/urh/util/util.py
+++ src/urh/util/util.py
@@ -13,3 +13,28 @@ def set_windows_lib_path():
         dll_dir = os.path.realpath(os.path.join(urh_dir, "dev", "native", "lib", "win", arch))
         print("Using DLLs from:", dll_dir)
         os.environ['PATH'] = os.environ['PATH'] + ";" + dll_dir
+
+
+def convert_bits_to_string(bits, output_view_type: int, pad_zeros=False):
+    if output_view_type == 0:
+        return "".join(map(str, map(int, bits)))
+    elif output_view_type == 1:
+        bits_str_list = list(map(str, map(int, bits)))
+
+        if pad_zeros:
+            bits_str_list += ["0"] * ((4 - (len(bits_str_list) % 4)) % 4)
+
+        return "".join(map(lambda h: "{0:x}".format(h),
+                           [int("".join(bits_str_list[i:i+4]), 2) for i in range(0, len(bits_str_list), 4)]))
+
+    elif output_view_type == 2:
+        bits_str_list = list(map(str, map(int, bits)))
+
+        if pad_zeros:
+            bits_str_list += ["0"] * ((8 - (len(bits_str_list) % 8)) % 8)
+
+        return "".join(map(chr,
+                           [int("".join(bits_str_list[i:i+8]), 2) for i in range(0, len(bits_str_list), 8)]))
+
+    elif output_view_type == 3:
+        return int("".join(map(str, map(int, bits))), 2)

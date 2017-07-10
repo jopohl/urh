c7e02a89acee9698e5df14b755bb4827477b4e78
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-07-10 10:07:51 +0200

    add icon to show selection in interpretation

--- src/urh/ui/views/ProtocolTableView.py
+++ src/urh/ui/views/ProtocolTableView.py
@@ -172,6 +172,7 @@ class ProtocolTableView(TableView):
 
         if not self.model().is_writeable:
             show_interpretation_action = menu.addAction(self.tr("Show selection in Interpretation"))
+            show_interpretation_action.setIcon(QIcon.fromTheme("zoom-select"))
             show_interpretation_action.triggered.connect(self.on_show_in_interpretation_action_triggered)
 
         if self.model().is_writeable:

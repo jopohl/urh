510c282a65c45a3aa21043fdd9c028b29408ca51
Andreas Noack <andreas.noack@fh-stralsund.de>
2017-07-11 19:18:50 +0200

    Adapted homematic.c and binary to new data dewhitening. For Homematic use CC1101 whitening + external programm (e.g. /tmp/homematic d and /tmp/homematic e), Without homematic "decryption", CRC can be checked (CRC-16 with CC1101 polynomial) correctly, after it there will be no checking and CRC set to 0x0000

--- data/decodings/homematic.c
+++ data/decodings/homematic.c
@@ -21,9 +21,11 @@ void print_binary(byte inpt)
 
 int main(int argc, char **argv)
 {
-    int i, max;
+    int i, max, offset;
     byte dec[256]={0}, enc[256]={0};
     char string[2048]={0};
+    
+    offset = 8; // Preamble + Sync
     
     if (argc>2)
     {
@@ -32,9 +34,10 @@ int main(int argc, char **argv)
             if (strlen(argv[2]) > 256*8 ) return -1;
             memcpy(string, argv[2], strlen(argv[2]));
             
-            for (i = 0; i < strlen(string); i+=8)
-                enc[i/8] = str2byte(&string[i]);
+            for (i = 0; i < strlen(string)-3; i+=8)
+                enc[i/8] = str2byte(&string[i]);                
             max = i/8;
+            memcpy(&dec, &enc, 256);
             
             /*
              * byte[] Dec = new byte[Enc.Length];
@@ -46,12 +49,15 @@ int main(int argc, char **argv)
              * Dec[j] = (byte)(Enc[j] ^ Dec[2]);
              */
             
-            dec[0] = enc[0];
+            dec[offset+0] = enc[offset+0];
             
-            dec[1] = (~enc[1])^0x89;
-            for(i = 2; i < max; i++)
+            dec[offset+1] = (~enc[offset+1])^0x89;
+            for(i = offset+2; i < max; i++)
                 dec[i] = (enc[i-1]+0xdc) ^ enc[i];
-            dec[i] = enc[i] ^ dec[2];
+            dec[offset+i] = enc[offset+i] ^ dec[offset+2];
+            
+            dec[max-1]=0; // Set CRC to 0x0000
+            dec[max-2]=0;
             
             for(i = 0; i < max; i++)
                 print_binary(dec[i]);
@@ -61,9 +67,10 @@ int main(int argc, char **argv)
             if (strlen(argv[2]) > 256*8 ) return -1;
             memcpy(string, argv[2], strlen(argv[2]));
             
-            for (i = 0; i < strlen(string); i+=8)
+            for (i = 0; i < strlen(string)-3; i+=8)
                 dec[i/8] = str2byte(&string[i]);
             max = i/8;
+            memcpy(&enc, &dec, 256);
             
             /*
              * byte[] Dec = new byte[Enc.Length];
@@ -75,12 +82,15 @@ int main(int argc, char **argv)
              * Dec[j] = (byte)(Enc[j] ^ Dec[2]);
              */
             
-            enc[0] = dec[0];
+            enc[offset+0] = dec[offset+0];
             
-            enc[1] = ~(dec[1]^0x89);
-            for(i = 2; i < max; i++)
+            enc[offset+1] = ~(dec[offset+1]^0x89);
+            for(i = offset+2; i < max; i++)
                 enc[i] = (enc[i-1]+0xdc) ^ dec[i];
-            enc[i] = dec[i] ^ dec[2];
+            enc[offset+i] = dec[offset+i] ^ dec[offset+2];
+            
+            enc[max-1]=0; // Set CRC to 0x0000
+            enc[max-2]=0;
             
             for(i = 0; i < max; i++)
                 print_binary(enc[i]);    

74dd0bcf2a53cdd69cbde993097c1bbfc9008336
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-11-18 14:10:59 +0100

    Improve handling of WAV files (#362)
    * refactor signal file loading
    
    * remove "alread quad demod" question
    
    * add handling for two channel wavs
    
    * add unittests for loading wavs

--- tests/test_maincontroller_gui.py
+++ tests/test_maincontroller_gui.py
@@ -1,6 +1,8 @@
 import os
 import tempfile
+import wave
 
+import numpy as np
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QApplication
 
@@ -87,3 +89,36 @@ class TestMaincontrollerGUI(QtTestCase):
 
         os.remove(self.get_path_for_filename("csvtest.complex"))
         os.remove(self.get_path_for_filename("csvtest_1.complex"))
+
+
+    def test_load_single_channel_wav(self):
+        filename = os.path.join(tempfile.gettempdir(), "test_single_channel.wav")
+        f = wave.open(filename, "w")
+        f.setnchannels(1)
+        f.setsampwidth(1)
+        f.setframerate(2e6)
+        f.writeframes(np.array([1, 2, 3, 4], dtype=np.uint8))
+        f.close()
+
+        self.add_signal_to_form(filename)
+        sig_frame = self.form.signal_tab_controller.signal_frames[0]
+        self.assertEqual(sig_frame.signal.sample_rate, 2e6)
+        self.assertEqual(sig_frame.signal.num_samples, 4)
+        self.assertNotEqual(sig_frame.signal.data.real.sum(), 0)
+        self.assertEqual(sig_frame.signal.data.imag.sum(), 0)
+
+    def test_load_stereo_wav(self):
+        filename = os.path.join(tempfile.gettempdir(), "test_stereo.wav")
+        f = wave.open(filename, "w")
+        f.setnchannels(2)
+        f.setsampwidth(2)
+        f.setframerate(10e6)
+        f.writeframes(np.array([23556, 2132, 111, 11111, 3455, 2324], dtype=np.int16))
+        f.close()
+
+        self.add_signal_to_form(filename)
+        sig_frame = self.form.signal_tab_controller.signal_frames[0]
+        self.assertEqual(sig_frame.signal.sample_rate, 10e6)
+        self.assertEqual(sig_frame.signal.num_samples, 3)
+        self.assertNotEqual(sig_frame.signal.data.real.sum(), 0)
+        self.assertNotEqual(sig_frame.signal.data.imag.sum(), 0)

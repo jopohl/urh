0fde06390848c5a1f4f14b349e58f1f7c963e72e
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-11-27 19:39:46 +0100

    fix data avail

--- src/urh/util/LinearBuffer.py
+++ src/urh/util/LinearBuffer.py
@@ -6,15 +6,19 @@ class LinearBuffer(object):
         self.data = np.zeros(size, dtype=np.complex64)
         self.write_current = 0
         self.read_current = 0
+        self.__data_available = 0
 
     def data_available(self, number):
-        return self.write_current >= ((self.read_current + number) % len(self.data))
+        return number <= self.__data_available
 
     def push(self, data):
         np.put(self.data, range(self.write_current, self.write_current+len(data)), data, mode="wrap")
         self.write_current = (self.write_current + len(data)) % len(self.data)
+        self.__data_available += len(data)
 
     def pop(self, number):
         result = self.data[self.read_current:self.read_current+number]
         self.read_current = (self.read_current + number) % len(self.data)
+        self.__data_available -= number
+        self.__data_available = max(0, self.__data_available)
         return result

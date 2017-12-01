3c3ab4636a4d506db30623615e5e8dbef105e46f
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-12-01 13:25:52 +0100

    move frozen interpreter fix so it also works without gnu radio path

--- src/urh/dev/gr/scripts/airspy_recv.py
+++ src/urh/dev/gr/scripts/airspy_recv.py
@@ -10,13 +10,14 @@ import tempfile
 import os
 import sys
 
+# Append script path at end to prevent conflicts in case of frozen interpreter
+sys.path.append(sys.path.pop(0))
+
 try:
     with open(os.path.join(tempfile.gettempdir(), "gnuradio_path.txt"), "r") as f:
         gnuradio_path = f.read().strip()
 
     os.environ["PATH"] = os.path.join(gnuradio_path, "bin")
-    # Append script path at end to prevent conflicts in case of frozen interpreter
-    sys.path.append(sys.path.pop(0))
     sys.path.insert(0, os.path.join(gnuradio_path, "lib", "site-packages"))
 
 except IOError:

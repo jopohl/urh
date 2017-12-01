331827f8b83d543eb8e1b098bdaf9cefb6a78cff
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-12-01 21:48:27 +0100

    centralize init path method

--- src/urh/dev/gr/scripts/usrp_send.py
+++ src/urh/dev/gr/scripts/usrp_send.py
@@ -6,23 +6,9 @@
 ##################################################
 
 from optparse import OptionParser
+import Initializer
 
-import tempfile
-import os
-import sys
-
-# Append script path at end to prevent conflicts in case of frozen interpreter
-sys.path.append(sys.path.pop(0))
-
-try:
-    with open(os.path.join(tempfile.gettempdir(), "gnuradio_path.txt"), "r") as f:
-        gnuradio_path = f.read().strip()
-
-    os.environ["PATH"] = os.path.join(gnuradio_path, "bin")
-    sys.path.insert(0, os.path.join(gnuradio_path, "lib", "site-packages"))
-
-except IOError:
-    pass
+Initializer.init_path()
 
 from gnuradio import gr
 from gnuradio import uhd

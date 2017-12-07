bc16884f9a41441e14cd692d6480d38b8daf0474
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-12-07 17:26:21 +0100

    add join timeout

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -138,9 +138,11 @@ class TestSimulator(QtTestCase):
 
         self.network_sdr_plugin_sender.send_raw_data(modulator.modulated_samples, 1)
         QTest.qWait(0)
-        receive_process.join()
+        receive_process.join(10)
 
         print("PROCESS TIME: {0:.2f}ms".format(elapsed.value))
+
+        self.assertLess(elapsed.value, 200)
 
         #timeout = spy.wait(2000)
         #yappi.get_func_stats().print_all()

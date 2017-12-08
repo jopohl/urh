dc752fb6d344f3a0ccd99d61f23d12aceb98a4e9
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-12-08 12:58:54 +0100

    remove misc folder

--- misc/USRP_init.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from subprocess import call
-
-# call(["ifconfig", "eth0", "192.168.10.1"])
-call("sysctl -w net.core.rmem_max=50000000", shell = True)
-call("sysctl -w net.core.rmem_max=50000000", shell = True)
-call("sysctl -w net.core.wmem_max=1048576", shell = True)

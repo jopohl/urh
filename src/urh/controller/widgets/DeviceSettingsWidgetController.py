f824fd54185b8717f878c67a17d6934b1cd5c491
Johannes Pohl <Johannes.Pohl90@gmail.com>
2018-01-13 11:52:38 +0100

    add device settings widget

--- src/urh/controller/widgets/DeviceSettingsWidgetController.py
+++ src/urh/controller/widgets/DeviceSettingsWidgetController.py
@@ -1,9 +1,12 @@
 from PyQt5.QtWidgets import QWidget
 from urh.ui.ui_send_recv_device_settings import Ui_FormDeviceSettings
+from urh.util.ProjectManager import ProjectManager
 
 
 class DeviceSettingsWidgetController(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, project_manager: ProjectManager, parent=None):
         super().__init__(parent)
         self.ui = Ui_FormDeviceSettings()
         self.ui.setupUi(self)
+
+

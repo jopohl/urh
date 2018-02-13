6e64d94267074c93d1201b6d75e3373fc18f03b9
andynoack <andreas.noack@hochschule-stralsund.de>
2018-02-13 16:41:16 +0100

    Fix sample rates for AirSpy - R2: 10M only - Mini: 6M only

--- src/urh/dev/config.py
+++ src/urh/dev/config.py
@@ -79,8 +79,8 @@ DEVICE_CONFIG[NetworkSDRInterfacePlugin.NETWORK_SDR_NAME] = {}
 # http://www.rtl-sdr.com/review-airspy-vs-sdrplay-rsp-vs-hackrf/
 DEVICE_CONFIG["AirSpy R2"] = {
     "center_freq": dev_range(start=24, stop=1800 * M, step=1),
-    "sample_rate": [2.5*M, 10*M],
-    "bandwidth": [2.5*M, 10*M],
+    "sample_rate": [10*M, 10*M],
+    "bandwidth": [10*M, 10*M],
     "rx_rf_gain":  list(range(0, 16)),
     "rx_if_gain":  list(range(0, 16)),
     "rx_baseband_gain":  list(range(0, 16)),
@@ -88,8 +88,8 @@ DEVICE_CONFIG["AirSpy R2"] = {
 
 DEVICE_CONFIG["AirSpy Mini"] = {
     "center_freq": dev_range(start=24, stop=1800 * M, step=1),
-    "sample_rate": [3*M, 6*M],
-    "bandwidth": [3*M, 6*M],
+    "sample_rate": [6*M, 6*M],
+    "bandwidth": [6*M, 6*M],
     "rx_rf_gain":  list(range(0, 16)),
     "rx_if_gain":  list(range(0, 16)),
     "rx_baseband_gain":  list(range(0, 16)),

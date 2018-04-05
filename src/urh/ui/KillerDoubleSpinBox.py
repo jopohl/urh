27fe92e19ed33cf8f8fc82b525b106e3b9de089e
Johannes Pohl <Johannes.Pohl90@gmail.com>
2018-04-05 17:48:04 +0200

    fix #421

--- src/urh/ui/KillerDoubleSpinBox.py
+++ src/urh/ui/KillerDoubleSpinBox.py
@@ -56,7 +56,7 @@ class KillerDoubleSpinBox(QDoubleSpinBox):
         elif text.endswith("K") or text.endswith("k"):
             return QLocale().toDouble(text[:-1])[0] * 10 ** 3
         else:
-            return QLocale().toDouble(text[:-1])[0]
+            return QLocale().toDouble(text)[0]
 
     def validate(self, inpt: str, pos: int):
         rx = QRegExp("^(-?[0-9]+)[.]?[0-9]*[kKmMgG]?$")

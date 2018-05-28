900a3232f17c5f1f8122fdd2fdaa5509ad2ce4c5
Johannes Pohl <Johannes.Pohl90@gmail.com>
2018-05-28 18:09:04 +0200

    add tests

--- tests/cli/test_cli_parsing.py
+++ tests/cli/test_cli_parsing.py
@@ -1,5 +1,6 @@
-import argparse
 import unittest
+from urh.dev.VirtualDevice import Mode
+from urh.dev.BackendHandler import Backends
 
 from urh.cli import urh_cli
 
@@ -47,7 +48,6 @@ class TestCLIParsing(unittest.TestCase):
         self.assertEqual(modulator.param_for_one, -20e3)
 
     def test_build_backend_handler_from_args(self):
-        from urh.dev.BackendHandler import Backends
         args = self.parser.parse_args("--device USRP --frequency 433.92e6 --sample-rate 2e6".split())
         bh = urh_cli.build_backend_handler_from_args(args)
         self.assertEqual(bh.device_backends["usrp"].selected_backend, Backends.native)
@@ -64,8 +64,6 @@ class TestCLIParsing(unittest.TestCase):
 
 
     def test_build_device_from_args(self):
-        from urh.dev.VirtualDevice import Mode
-        from urh.dev.BackendHandler import Backends
         args = self.parser.parse_args("--device HackRF --frequency 433.92e6 --sample-rate 2e6".split())
         with self.assertRaises(ValueError):
             urh_cli.build_device_from_args(args)
@@ -107,3 +105,29 @@ class TestCLIParsing(unittest.TestCase):
         self.assertEqual(device.backend, Backends.grc)
         self.assertEqual(device.frequency, 133.7e6)
         self.assertEqual(device.mode, Mode.send)
+
+    def test_build_protocol_sniffer_from_args(self):
+        args = self.parser.parse_args("--device HackRF --frequency 50e3 --sample-rate 2.5e6 -rx "
+                                      "-if 24 -bb 30 -g 0 --device-identifier abcde "
+                                      "-bl 1337 --center 0.5 --noise 0.1234 --tolerance 42".split())
+        sniffer = urh_cli.build_protocol_sniffer_from_args(args)
+        self.assertEqual(sniffer.rcv_device.frequency, 50e3)
+        self.assertEqual(sniffer.rcv_device.sample_rate, 2.5e6)
+        self.assertEqual(sniffer.rcv_device.bandwidth, 2.5e6)
+        self.assertEqual(sniffer.rcv_device.name, "hackrf")
+        self.assertEqual(sniffer.rcv_device.backend, Backends.native)
+        self.assertEqual(sniffer.rcv_device.mode, Mode.receive)
+        self.assertEqual(sniffer.rcv_device.if_gain, 24)
+        self.assertEqual(sniffer.rcv_device.gain, 0)
+        self.assertEqual(sniffer.rcv_device.baseband_gain, 30)
+        self.assertEqual(sniffer.rcv_device.device_serial, "abcde")
+        self.assertEqual(sniffer.signal.bit_len, 1337)
+        self.assertEqual(sniffer.signal.noise_threshold, 0.1234)
+        self.assertEqual(sniffer.signal.qad_center, 0.5)
+        self.assertEqual(sniffer.signal.tolerance, 42)
+
+
+    def test_build_encoding_from_args(self):
+        args = self.parser.parse_args('--device HackRF --frequency 50e3 --sample-rate 2.5e6 -e "Test,Invert"'.split())
+        encoding = urh_cli.build_encoding_from_args(args)
+        self.assertEqual(len(encoding.chain), 2)

f1250b7c2527278ceef0f6bad443dfcb10023506
Johannes Pohl <Johannes.Pohl90@gmail.com>
2018-06-20 18:59:10 +0200

    add qwait so model data gets called during test

--- tests/test_options_gui.py
+++ tests/test_options_gui.py
@@ -1,3 +1,5 @@
+from PyQt5.QtTest import QTest
+
 from tests.QtTestCase import QtTestCase
 from urh.controller.dialogs.OptionsDialog import OptionsDialog
 from urh.dev.BackendHandler import BackendHandler
@@ -42,6 +44,8 @@ class TestOptionsGUI(QtTestCase):
 
     def test_device_tab(self):
         self.dialog.ui.tabWidget.setCurrentIndex(4)
+        # ensure device table model gets populated during tests
+        QTest.qWait(300)
         self.assertEqual(self.dialog.ui.tabWidget.tabText(4), "Device")
 
         self.assertEqual(self.dialog.ui.tblDevices.model().rowCount(), len(BackendHandler.DEVICE_NAMES))

d6b38ad7052f8cd328af0947ca00aabe40105f2c
Johannes Pohl <Johannes.Pohl90@gmail.com>
2018-07-24 20:56:30 +0200

    fix unittest for numpy 1.15

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -90,23 +90,18 @@ class TestFilter(QtTestCase):
         self.assertTrue(np.array_equal(result_np, expected_result))
 
         result_fft = Filter.fft_convolve_1d(x, h)
-        self.assertTrue(np.array_equal(result_fft, expected_result))
+        self.assertEqual(len(expected_result), len(result_fft))
+        for i in range(len(expected_result)):
+            self.assertAlmostEqual(expected_result[i], result_fft[i], places=8, msg=str(i))
 
         x = np.linspace(0, 1, num=10 ** 3).astype(np.complex64)
         h = Filter.design_windowed_sinc_bandpass(0.1, 0.4, 0.01)
         # fft convolve is faster if IR is round about 400 samples or windowed sinc has bandwidth of 0.01
-        #print(len(h))
 
-        t_np = time.time()
         result_np = np.convolve(x, h, mode="same")
-        t_np = time.time() - t_np
-
-        t_fft = time.time()
         result_fft = Filter.fft_convolve_1d(x, h)
-        t_fft = time.time() - t_fft
 
         np.testing.assert_array_almost_equal(result_np, result_fft)
-        #print("fft convolve time", t_fft, "np convolve time", t_np)
 
     def test_bandpass_filter(self):
         # GUI tests for bandpass filter are in test_spectrogram.py

0fa448549685d24a4c13b5ab0dceecbb265f4412
Johannes Pohl <Johannes.Pohl90@gmail.com>
2018-07-30 15:13:33 +0200

    reorder tests

--- tests/auto_interpretation/test_auto_interpretation_integration.py
+++ tests/auto_interpretation/test_auto_interpretation_integration.py
@@ -2,31 +2,25 @@ import unittest
 
 import numpy as np
 
-from urh.ainterpretation import AutoInterpretation
 from tests.test_util import get_path_for_data_file
+from urh.ainterpretation import AutoInterpretation
+
 
 class TestAutoInterpretationIntegration(unittest.TestCase):
     def test_auto_interpretation_fsk(self):
         fsk_signal = np.fromfile(get_path_for_data_file("fsk.complex"), dtype=np.complex64)
         AutoInterpretation.estimate(fsk_signal)
 
+        # noise = 0.0009 // center ~= -0.0234 // bit len = 100
 
-    def test_get_bit_length_from_magnitudes(self):
+    def test_auto_interpretation_ask(self):
+        ask_signal = np.fromfile(get_path_for_data_file("ask.complex"), dtype=np.complex64)
+        AutoInterpretation.estimate(ask_signal)
 
-        pass
-        #fsk_signal = np.fromfile("fsk.complex", dtype=np.complex64)
+        # noise ~= 0.020 // center ~= 0 // bit len = 300
 
+    def test_auto_interpretation_enocean(self):
+        enocean_signal = np.fromfile(get_path_for_data_file("enocean.complex"), dtype=np.complex64)
+        AutoInterpretation.estimate(enocean_signal)
 
-        #message_indices = AutoInterpretation.segment_messages_from_magnitudes(np.abs(fsk_signal), 0.0009, q=2)
-        #demod_signal = np.fromfile("fsk-demodulated.complex", dtype=np.float32)
-        #self.assertEqual(AutoInterpretation.get_bit_length_from_signal(demod_signal, message_indices, center=-0.0234), 100)
-
-        #ask_signal = np.fromfile("ask.complex", dtype=np.complex64)
-        #message_indices = AutoInterpretation.segment_messages_from_magnitudes(np.abs(ask_signal), 0.020, q=2)
-        #demod_signal = np.fromfile("ask-demodulated.complex", dtype=np.float32)
-        #self.assertEqual(AutoInterpretation.get_bit_length_from_signal(demod_signal, message_indices, center=0), 300)
-
-        #enocean_signal = np.fromfile("enocean.complex", dtype=np.complex64)
-        #message_indices = AutoInterpretation.segment_messages_from_magnitudes(np.abs(enocean_signal), 0.025, q=2)
-        #demod_signal = np.fromfile("enocean-demodulated.complex", dtype=np.float32)
-        #self.assertEqual(AutoInterpretation.get_bit_length_from_signal(demod_signal, message_indices, center=0.0837), 8)
+        # noise ~= 0.025 // center ~= 0.0837 // bit len = 8

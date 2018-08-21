571646a3babefea5f8f76c649e02acc4abd828ba
Johannes Pohl <Johannes.Pohl90@gmail.com>
2018-08-21 16:10:12 +0200

    prepare fsk test: generate messages and estimate parameters

--- tests/auto_interpretation/LabTestsAutoInterpretation.py
+++ tests/auto_interpretation/LabTestsAutoInterpretation.py
@@ -6,6 +6,7 @@ from typing import List
 
 import numpy as np
 
+from urh.ainterpretation import AutoInterpretation
 from urh.signalprocessing.Message import Message
 from urh.signalprocessing.Modulator import Modulator
 
@@ -52,14 +53,64 @@ class LabTestsAutoInterpretation(unittest.TestCase):
             for _ in range(num_messages):
                 f.write(self.generate_message_bits(num_bits, preamble, sync) + "\n")
 
+    def read_messages_from_file(self, filename: str, message_pause: int) -> List[Message]:
+        result = []
+        with open(filename, "r") as f:
+            for line in f:
+                result.append(Message.from_plain_bits_str(line.strip(), pause=message_pause))
+        return result
 
     def test_against_noise(self):
+        # file is only created once, delete it for fresh testdata
         target_file = os.path.join(tempfile.gettempdir(), "auto_int_against_noise_data.txt")
 
         if not os.path.isfile(target_file):
-            self.generate_random_messages(target_file, num_messages=1000, num_bits=80,
-                                          preamble="10101010", sync="11110000")
+            self.generate_random_messages(target_file, num_messages=30, num_bits=80,
+                                          preamble="1010101010101010", sync="11011001")
+            print("Bits written to {}".format(target_file))
 
+        messages = self.read_messages_from_file(target_file, message_pause=1000)
+
+        fsk_modulator = Modulator("FSK")
+        fsk_modulator.samples_per_bit = 100
+        fsk_modulator.modulation_type_str = "FSK"
+        fsk_modulator.param_for_zero = -20e3
+        fsk_modulator.param_for_one = 20e3
+        fsk_modulator.sample_rate = 100e3
+
+        ook_modulator = Modulator("OOK")
+        ook_modulator.samples_per_bit = 100
+        ook_modulator.modulation_type_str = "ASK"
+        ook_modulator.param_for_zero = 0
+        ook_modulator.param_for_one = 100
+        ook_modulator.sample_rate = 100e3
+
+        ask_modulator = Modulator("ASK")
+        ask_modulator.samples_per_bit = 100
+        ask_modulator.modulation_type_str = "ASK"
+        ask_modulator.param_for_zero = 50
+        ask_modulator.param_for_one = 100
+        ask_modulator.sample_rate = 100e3
+
+        psk_modulator = Modulator("PSK")
+        psk_modulator.samples_per_bit = 100
+        psk_modulator.modulation_type_str = "PSK"
+        psk_modulator.param_for_zero = 0
+        psk_modulator.param_for_one = 180
+        psk_modulator.sample_rate = 100e3
+
+        messages_per_signal = 5
+
+        fsk_signals = []   # type: List[np.ndarray]
+        for i in range(0, len(messages), messages_per_signal):
+            fsk_signals.append(self.generate_signal(messages[i:i+messages_per_signal], fsk_modulator, snr_db=20))
+
+        print("Estimating parameters for {} signals".format(len(fsk_signals)))
+        fsk_estimations = []
+        for signal in fsk_signals:
+            fsk_estimations.append(AutoInterpretation.estimate(signal))
+
+        print(fsk_estimations)
 
     def test_fsk(self):
         modulator = Modulator("")

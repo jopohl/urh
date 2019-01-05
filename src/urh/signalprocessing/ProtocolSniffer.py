65b269cd8b04e27ebddcde985f4fa46c6a4dfaf9
Johannes Pohl <Johannes.Pohl90@gmail.com>
2019-01-05 18:09:43 +0100

    remove debug

--- src/urh/signalprocessing/ProtocolSniffer.py
+++ src/urh/signalprocessing/ProtocolSniffer.py
@@ -187,9 +187,7 @@ class ProtocolSniffer(ProtocolAnalyzer, QObject):
 
         bit_len = self.signal.bit_len
         if self.automatic_center:
-            old_center = self.signal.qad_center
             self.signal.qad_center = AutoInterpretation.detect_center(self.signal.qad, max_size=150*self.signal.bit_len)
-            logger.debug("Setting center from {:.3f} to {:.3f}".format(old_center, self.signal.qad_center))
 
         ppseq = grab_pulse_lens(self.signal.qad, self.signal.qad_center,
                                 self.signal.tolerance, self.signal.modulation_type, self.signal.bit_len)

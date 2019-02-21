09fafbb7b2ed8a196a1d635456d2540eae50221e
Johannes Pohl <Johannes.Pohl90@gmail.com>
2019-02-21 21:21:03 +0100

    add icon to windows

--- data/pyinstaller_helper.py
+++ data/pyinstaller_helper.py
@@ -8,6 +8,8 @@ DATA = [("src/urh/dev/native/lib/shared", "."), ("src/urh/plugins", "urh/plugins
 EXCLUDE = ["matplotlib"]
 
 cmd = ["pyinstaller", "--windowed"]
+if sys.platform == "darwin":
+    cmd.append("--onefile")
 
 for hidden_import in HIDDEN_IMPORTS:
     cmd.append("--hidden-import={}".format(hidden_import))
@@ -22,6 +24,7 @@ for exclude in EXCLUDE:
 cmd.extend(["--distpath", "./dist-{}".format(64 if sys.maxsize > 2**32 else 32)])
 
 urh_path = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+cmd.append('--icon="{}"'.format(os.path.join(urh_path, "data/icons/appicon.ico")))
 cmd.append(os.path.join(urh_path, "src/urh/main.py"))
 cmd = " ".join(cmd)
 print(cmd)

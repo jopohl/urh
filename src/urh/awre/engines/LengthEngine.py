2a600a9fef69b07fafe82e521f314b0cc1713f50
Johannes Pohl <Johannes.Pohl90@gmail.com>
2019-04-18 21:20:41 +0200

    add print

--- src/urh/awre/engines/LengthEngine.py
+++ src/urh/awre/engines/LengthEngine.py
@@ -25,13 +25,11 @@ class LengthEngine(Engine):
             bin_num = int(math.ceil(len(bitvector) / n_gram_length))
             bitvectors_by_n_gram_length[bin_num].append(i)
 
-        print(list(map(len, self.bitvectors)))
-
-        # TODO: From here we should start a loop for subclustering
         common_ranges_by_length = self.find_common_ranges_by_cluster(self.bitvectors, bitvectors_by_n_gram_length)
         for length, ranges in common_ranges_by_length.items():
             common_ranges_by_length[length] = self.ignore_already_labeled(ranges, self.already_labeled)
 
+        print(self.already_labeled)
         self.filter_common_ranges(common_ranges_by_length)
         self._debug("Common Ranges:", common_ranges_by_length)
 

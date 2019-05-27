1bf70b8e56f147f029d41726e4c8d2d1ab844260
Johannes Pohl <Johannes.Pohl90@gmail.com>
2019-05-27 12:44:21 +0200

    fix cont modulator

--- src/urh/signalprocessing/ContinuousModulator.py
+++ src/urh/signalprocessing/ContinuousModulator.py
@@ -26,7 +26,7 @@ class ContinuousModulator(object):
         self.modulators = modulators
         self.num_repeats = num_repeats  # -1 or 0 = infinite
 
-        self.ring_buffer = RingBuffer(int(constants.CONTINUOUS_BUFFER_SIZE_MB*10**6)//8)
+        self.ring_buffer = RingBuffer(int(constants.CONTINUOUS_BUFFER_SIZE_MB*10**6)//8, dtype=Modulator.get_dtype())
 
         self.current_message_index = Value("L", 0)
 

a64b94ebd8b7c7e7c8dc678122185870dd178bf9
Johannes Pohl <Johannes.Pohl90@gmail.com>
2019-06-24 17:58:27 +0200

    fix ringbuffer init

--- src/urh/dev/EndlessSender.py
+++ src/urh/dev/EndlessSender.py
@@ -24,6 +24,7 @@ class EndlessSender(object):
     def device(self, value: VirtualDevice):
         self.__device = value
         self.__device.is_send_continuous = True
+        self.ringbuffer = RingBuffer(int(constants.CONTINUOUS_BUFFER_SIZE_MB * 10 ** 6) // 8, self.__device.data_type)
         self.__device.continuous_send_ring_buffer = self.ringbuffer
 
     @property

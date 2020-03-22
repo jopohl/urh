fea948caac810ce6c45b4a8835c27b6015be93f0
Johannes Pohl <Johannes.Pohl90@gmail.com>
2020-03-22 18:21:33 +0100

    update script

--- data/install_manylinux2014_deps.sh
+++ data/install_manylinux2014_deps.sh
@@ -5,7 +5,7 @@ BLADERF_VERSION="2019.07"
 LIMESUITE_VERSION="20.01.0"
 SDRPLAY_VERSION="2.13"
 
-yum -y install wget cmake3 rtl-sdr-devel hackrf-devel uhd-devel
+yum -y install wget cmake cmake3 rtl-sdr-devel hackrf-devel uhd-devel
 yum -y install https://github.com/analogdevicesinc/libiio/releases/download/v0.19/libiio-0.19.g5f5af2e-centos-7-x86_64.rpm
 
 cd /tmp
@@ -13,14 +13,14 @@ wget https://github.com/airspy/airspyone_host/archive/v$AIRSPY_VERSION.tar.gz
 tar xf v$AIRSPY_VERSION.tar.gz && rm v$AIRSPY_VERSION.tar.gz
 cd /tmp/airspyone_host-$AIRSPY_VERSION
 mkdir build_airspy && cd build_airspy
-cmake3 .. && make -j$(nproc) && make install
+cmake .. && make -j$(nproc) && make install
 
 cd /tmp
 wget https://github.com/Nuand/bladeRF/archive/$BLADERF_VERSION.tar.gz
 tar xf $BLADERF_VERSION.tar.gz && rm v$BLADERF_VERSION.tar.gz
 cd /tmp/bladeRF-$BLADERF_VERSION
 mkdir build_bladeRF && cd build_bladeRF
-cmake3 .. && make -j$(nproc) && make install
+cmake .. && make -j$(nproc) && make install
 
 cd /tmp
 wget https://github.com/myriadrf/LimeSuite/archive/v$LIMESUITE_VERSION.tar.gz
@@ -31,7 +31,7 @@ cmake3 .. && make -j$(nproc) && make install
 
 
 cd /tmp
-wget http://www.sdrplay.com/software/SDRplay_RSP_API-Linux-$SDRPLAY_API_VERSION.1.run -O sdrplay.run
+wget http://www.sdrplay.com/software/SDRplay_RSP_API-Linux-$SDRPLAY_VERSION.1.run -O sdrplay.run
 bash sdrplay.run --tar xf
 mv mirsdrapi-rsp.h /usr/include
 mv x86_64/* /usr/lib

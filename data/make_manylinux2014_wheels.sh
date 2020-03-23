a2a9a2dab39518ec9ea564b73ce918fef74b7739
Johannes Pohl <Johannes.Pohl90@gmail.com>
2020-03-23 19:37:02 +0100

    improvements

--- data/make_manylinux2014_wheels.sh
+++ data/make_manylinux2014_wheels.sh
@@ -60,14 +60,14 @@ wait
 
 touch /tmp/urh_releasing
 for PYBIN in /opt/python/*/bin; do
-    echo -e "\033[92mInstalling requirements for \033[1m$PYBIN\033[0m"
+    echo -e "\033[1mInstalling requirements for $PYBIN\033[0m"
     "${PYBIN}/pip" install -r /io/data/requirements.txt > /dev/null
 
     cd /io || return
-    echo -e "\033[92mBuilding extentions for \033[1m$PYBIN\033[0m"
+    echo -e "\033[1mBuilding extentions for $PYBIN\033[0m"
     "${PYBIN}/python3" setup.py build_ext "-j$(nproc)" | grep --color=always "Skipping"
 
-    echo -e "\033[92mBuilding wheel for \033[1m$PYBIN\033[0m"
+    echo -e "\033[1mBuilding wheel for $PYBIN\033[0m"
     "${PYBIN}/pip" wheel --no-deps /io/ -w /wheelhouse/ > /dev/null
 done
 

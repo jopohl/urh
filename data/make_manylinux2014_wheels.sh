8c344cc40ac0712a3c9d158d9af70ccb77efab53
Johannes Pohl <Johannes.Pohl90@gmail.com>
2020-03-23 14:50:54 +0100

    improve script

--- data/make_manylinux2014_wheels.sh
+++ data/make_manylinux2014_wheels.sh
@@ -4,17 +4,16 @@ AIRSPY_VERSION="1.0.9"
 BLADERF_VERSION="2018.08"
 LIMESUITE_VERSION="20.01.0"
 SDRPLAY_VERSION="2.13"
+RTLSDR_VERSION="0.6.0"
 
 echo -e '\033[92mInstalling dependencies...\033[0m'
-start=$SECONDS
-yum -y -q install wget cmake3 rtl-sdr-devel hackrf-devel uhd-devel\
+yum -y -q install wget cmake3 hackrf-devel uhd-devel\
        https://github.com/analogdevicesinc/libiio/releases/download/v0.19/libiio-0.19.g5f5af2e-centos-7-x86_64.rpm
-echo -e "\033[93mInstalling dependencies took $(( $SECONDS - start )) seconds\033[0m"
 
 build_airspy() {
   wget https://github.com/airspy/airspyone_host/archive/v$AIRSPY_VERSION.tar.gz -O /tmp/airspy.tar.gz &> /dev/null
   tar xf /tmp/airspy.tar.gz -C /tmp
-  cmake3 -S /tmp/airspyone_host-$AIRSPY_VERSION -B /tmp/build_airspy > /dev/null
+  cmake3 -Wno-dev -S /tmp/airspyone_host-$AIRSPY_VERSION -B /tmp/build_airspy > /dev/null
   make --silent -C /tmp/build_airspy > /dev/null
   make --silent -C /tmp/build_airspy install > /dev/null
 }
@@ -22,7 +21,7 @@ build_airspy() {
 build_bladerf() {
   wget https://github.com/Nuand/bladeRF/archive/$BLADERF_VERSION.tar.gz -O /tmp/bladeRF.tar.gz &> /dev/null
   tar xf /tmp/bladeRF.tar.gz -C /tmp
-  cmake3 -S /tmp/bladeRF-$BLADERF_VERSION -B /tmp/build_blade > /dev/null
+  cmake3 -Wno-dev -S /tmp/bladeRF-$BLADERF_VERSION/host -B /tmp/build_blade > /dev/null
   make --silent -C /tmp/build_blade > /dev/null
   make --silent -C /tmp/build_blade install > /dev/null
 }
@@ -30,9 +29,17 @@ build_bladerf() {
 build_limesdr() {
   wget https://github.com/myriadrf/LimeSuite/archive/v$LIMESUITE_VERSION.tar.gz -O /tmp/lime.tar.gz &> /dev/null
   tar xf /tmp/lime.tar.gz -C /tmp
-  cmake3 -S /tmp/LimeSuite-$LIMESUITE_VERSION -B /tmp/build_lime > /dev/null
+  cmake3 -Wno-dev -S /tmp/LimeSuite-$LIMESUITE_VERSION -B /tmp/build_lime > /dev/null
   make --silent -C /tmp/build_lime > /dev/null
   make --silent -C /tmp/build_lime install > /dev/null
+}
+
+build_rtlsdr() {
+  wget https://github.com/osmocom/rtl-sdr/archive/$RTLSDR_VERSION.tar.gz -O /tmp/rtlsdr.tar.gz &> /dev/null
+  tar xf /tmp/rtlsdr.tar.gz -C /tmp
+  cmake3 -Wno-dev -S /tmp/rtl-sdr-$RTLSDR_VERSION -B /tmp/build_rtlsdr > /dev/null
+  make --silent -C /tmp/build_rtlsdr > /dev/null
+  make --silent -C /tmp/build_rtlsdr install > /dev/null
 }
 
 build_sdrplay() {
@@ -43,40 +50,28 @@ build_sdrplay() {
   ln -s /usr/lib64/libmirsdrapi-rsp.so.$SDRPLAY_VERSION /usr/lib64/libmirsdrapi-rsp.so
 }
 
-start=$SECONDS
+echo -e '\033[92mCompiling SDR libs...\033[0m'
 build_airspy &
 build_bladerf &
 build_limesdr &
+build_rtlsdr &
 build_sdrplay &
 wait
-echo -e "\033[93mCompiling SDR libraries took $(( $SECONDS - start )) seconds\033[0m"
 
-start=$SECONDS
 for PYBIN in /opt/python/*/bin; do
-    start1=$SECONDS
     echo -e "\033[36mInstalling requirements for $PYBIN\033[0m"
     "${PYBIN}/pip" install -r /io/data/requirements.txt > /dev/null
-    echo -e "\033[96mFinished after $(( $SECONDS - start1 )) seconds\033[0m"
 
-    start1=$SECONDS
     cd /io || return
     echo -e "\033[36mBuilding extentions for $PYBIN\033[0m"
-    "${PYBIN}/python3" setup.py build_ext "-j$(nproc)" > /dev/null
-    echo -e "\033[96mFinished after $(( $SECONDS - start1 )) seconds\033[0m"
+    "${PYBIN}/python3" setup.py build_ext "-j$(nproc)" | grep --color=always "Skipping"
 
-    start1=$SECONDS
     echo -e "\033[36mBuilding wheel for $PYBIN\033[0m"
     "${PYBIN}/pip" wheel --no-deps /io/ -w /wheelhouse/ > /dev/null
-    echo -e "\033[96mFinished after $(( $SECONDS - start1 )) seconds\033[0m"
 done
-
-echo -e "\033[93mBuilding wheels took $(( $SECONDS - start )) seconds\033[0m"
-wait
 
 # Bundle external libs into wheels
-start=$SECONDS
+echo -e '\033[92mReparing wheels...\033[0m'
 for whl in /wheelhouse/*.whl; do
-    auditwheel repair "$whl" -w /io/dist/ &
+    auditwheel repair "$whl" -w /io/dist/
 done
-wait
-echo -e "\033[93mRepairing wheels took $(( $SECONDS - start )) seconds\033[0m"

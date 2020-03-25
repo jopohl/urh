adc0138b537adbdd4e2855d26f78736c7882dc2c
Johannes Pohl <Johannes.Pohl90@gmail.com>
2020-03-25 11:36:03 +0100

    add color

--- data/check_native_backends.py
+++ data/check_native_backends.py
@@ -7,9 +7,9 @@ rc = 0
 for sdr in ("AirSpy", "BladeRF", "HackRF", "RTLSDR", "LimeSDR", "PlutoSDR", "SDRPlay", "USRP"):
     try:
         importlib.import_module('.{}'.format(sdr.lower()), 'urh.dev.native.lib')
-        print("{:<10} SUCCESS".format(sdr+":"))
+        print("{:<10} \033[92mSUCCESS\033[0m".format(sdr+":"))
     except ImportError as e:
-        print("{:<10} FAILURE ({})".format(sdr+":", e))
+        print("{:<10} \033[91mFAILURE\033[0m ({})".format(sdr+":", e))
         rc = 1
 
 sys.exit(rc)

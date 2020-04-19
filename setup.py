66cf6adcec9c45269260d45dbd8f10bb914a462f
Johannes Pohl <Johannes.Pohl90@gmail.com>
2020-04-19 11:06:47 +0200

    remove parallelization in ExtensionHelper.py it leads to errors on certain environments (#761)

--- setup.py
+++ setup.py
@@ -94,8 +94,7 @@ def get_extensions():
         for extension in extensions:
             extension.extra_compile_args.append(NO_NUMPY_WARNINGS_FLAG)
 
-    extensions = cythonize(extensions, compiler_directives=COMPILER_DIRECTIVES, compile_time_env=device_extras,
-                           nthreads=0 if sys.platform in ("win32", "darwin") else os.cpu_count())
+    extensions = cythonize(extensions, compiler_directives=COMPILER_DIRECTIVES, compile_time_env=device_extras)
     return extensions
 
 

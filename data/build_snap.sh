dd141b63e7a57186d9425f8738679a304eed5e79
jopohl <Johannes.Pohl90@gmail.com>
2020-04-19 14:30:14 +0200

    add snapcraft.yaml and build_snap.sh

---
+++ data/build_snap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=$(dirname "$(readlink -f "$0")")
+
+echo "Copy snapcraft.yaml to base directory"
+cd $DIR
+cp snapcraft.yaml ..
+cd ..
+sed -i "s/version\: git/version\: $(python3 src/urh/version.py)/" snapcraft.yaml
+
+
+SNAPCRAFT_BUILD_ENVIRONMENT_CPU=4 SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=5G snapcraft
+sudo snap install --dangerous urh*.snap
+urh

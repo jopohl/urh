01ce4d417e4c18eef52ddd8f786d8daeea37abf0
jopohl <Johannes.Pohl90@gmail.com>
2020-06-04 11:21:15 +0200

    cythonize de bruijn

--- src/urh/util/util.py
+++ src/urh/util/util.py
@@ -7,6 +7,7 @@ import sys
 import time
 from xml.dom import minidom
 from xml.etree import ElementTree as ET
+import urh.cythonext.util as c_util
 
 import numpy as np
 from PyQt5.QtCore import Qt
@@ -475,27 +476,4 @@ def calc_x_y_scale(rect, parent):
 
 
 def de_bruijn(n: int) -> array.array:
-    """
-    de Bruijn sequence for alphabet k and subsequences of length n.
-
-    https://www.wikiwand.com/en/De_Bruijn_sequence
-    """
-    alphabet = array.array("B", [0, 1])
-    k = len(alphabet)
-
-    a = [0] * k * n
-    sequence = []
-
-    def db(t, p):
-        if t > n:
-            if n % p == 0:
-                sequence.extend(a[1:p + 1])
-        else:
-            a[t] = a[t - p]
-            db(t + 1, p)
-            for j in range(a[t - p] + 1, k):
-                a[t] = j
-                db(t + 1, t)
-
-    db(1, 1)
-    return array.array("B", [alphabet[i] for i in sequence])
+    return c_util.de_bruijn(n)

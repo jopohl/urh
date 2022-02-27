95f1ee4d540f0d5baa041bfe49dd098696f0a718
Dr. Johannes Pohl <Johannes.Pohl90@gmail.com>
2022-02-27 18:20:38 +0100

    support windows in native backend check

--- data/check_native_backends.py
+++ data/check_native_backends.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 import importlib
+import os
 import sys
 rc = 0
+
+if sys.platform == "win32":
+    shared_lib_dir = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "src/dev/native/lib/shared"))
+    print("Attempting to read", shared_lib_dir)
+    if os.path.isdir(shared_lib_dir):
+        os.environ["PATH"] = os.environ.get("PATH", "") + os.pathsep + shared_lib_dir
+        print("PATH updated")
 
 for sdr in ("AirSpy", "BladeRF", "HackRF", "RTLSDR", "LimeSDR", "PlutoSDR", "SDRPlay", "USRP"):
     try:

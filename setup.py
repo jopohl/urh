d9a68fac3bfeb0300cf2975e713ab1cd1cb37b8a
Rafael Fontenelle <rafaelff@gnome.org>
2024-03-11 13:30:25 -0300

    Handle module __builtins__ both as dict or module

--- setup.py
+++ setup.py
@@ -50,13 +50,18 @@ except ImportError:
     )
     sys.exit(1)
 
+def set_builtin(name, value):
+    if isinstance(__builtins__, dict):
+        __builtins__[name] = value
+    else:
+        setattr(__builtins__, name, value)
 
 class build_ext(_build_ext):
     def finalize_options(self):
         print("Finalizing options")
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        set_builtin('__NUMPY_SETUP__', False)
         import numpy
 
         self.include_dirs.append(numpy.get_include())

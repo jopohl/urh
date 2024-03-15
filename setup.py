2f565c7544c71c4ee73d8d24167f546ca9aac363
Rafael Fontenelle <rffontenelle@users.noreply.github.com>
2024-03-15 05:28:58 -0300

    Handle module __builtins__ both as dict or module (#1107)

--- setup.py
+++ setup.py
@@ -51,12 +51,21 @@ except ImportError:
     sys.exit(1)
 
 
+def set_builtin(name, value):
+    if isinstance(__builtins__, dict):
+        __builtins__[name] = value
+    else:
+        # to support https://github.com/pypa/build
+        # see https://github.com/jopohl/urh/issues/1106
+        setattr(__builtins__, name, value)
+
+
 class build_ext(_build_ext):
     def finalize_options(self):
         print("Finalizing options")
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        set_builtin("__NUMPY_SETUP__", False)
         import numpy
 
         self.include_dirs.append(numpy.get_include())

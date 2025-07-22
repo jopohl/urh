479ef095e7055100c1ef0610fc06bb11bdd07528
FlUxIuS <fluxius@gmail.com>
2025-07-22 17:13:24 +0200

    Readapting implmentation to the new prepare_sync_receive method

--- src/urh/dev/native/USRP.py
+++ src/urh/dev/native/USRP.py
@@ -74,7 +74,7 @@ class USRP(Device):
         return True
 
     @classmethod
-    def prepare_sync_receive(cls, ctrl_connection: Connection):
+    def prepare_sync_receive(cls, ctrl_connection: Connection, dev_parameters: OrderedDict):
         ctrl_connection.send("Initializing stream...")
         usrp.setup_stream()
         return usrp.start_stream(cls.SYNC_RX_CHUNK_SIZE)
